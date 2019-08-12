92af0ce23b8d1d2969a150e4165daea9f719e871
ozelenyi <oleksandr.zelenyi@jevera.software>
2019-08-12 11:13:59 +0300

    Core. Logs. Get logs by ms

--- src/app/admin/logs/logs.component.ts
+++ src/app/admin/logs/logs.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { JhiLanguageService } from 'ng-jhipster';
 
 import { Log } from './log.model';
 import { LogsService } from './logs.service';
+import { JhiHealthService } from '../health/health.service';
 
 @Component({
     selector: 'xm-logs',
@@ -13,10 +13,14 @@ export class LogsComponent implements OnInit {
     loggers: Log[];
     filter: string;
     orderProp: string;
+    showLoader: boolean;
     reverse: boolean;
+    selectedService = '';
+    services: any[];
 
     constructor(
-        private logsService: LogsService
+        private logsService: LogsService,
+        private healthService: JhiHealthService
     ) {
         this.filter = '';
         this.orderProp = 'name';
@@ -24,13 +28,41 @@ export class LogsComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.logsService.findAll().subscribe((loggers) => this.loggers = loggers.body);
+        this.healthService
+            .getMonitoringServicesCollection()
+            .subscribe(result => {
+                this.services = result || [];
+                if (this.services.length > 0) {
+                    this.selectedService = this.services[1].name;
+                    this.onServiceSelect();
+                }
+            }, error => console.log(error));
     }
 
-    changeLevel(name: string, level: string) {
+    getLoggers() {
+        this.logsService
+            .findByService(this.selectedService)
+            .subscribe((loggers) => {
+                this.loggers = loggers.body || [];
+                this.showLoader = false;
+            }, error => {
+                console.error(error);
+                this.showLoader = false;
+                this.loggers = null;
+            });
+    }
+
+    onServiceSelect(): void {
+        this.loggers = [];
+        this.showLoader = true;
+        this.getLoggers();
+    }
+
+    changeLevel(name: string, level: string): void {
+        this.showLoader = true;
         const log = new Log(name, level);
-        this.logsService.changeLevel(log).subscribe(() => {
-            this.logsService.findAll().subscribe((loggers) => this.loggers = loggers.body);
-        });
+        this.logsService
+            .changeLevel(log, this.selectedService)
+            .subscribe(() => this.getLoggers());
     }
 }
