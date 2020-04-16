33b201a71d6d822b4997c891f423bd27b20220b2
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2020-04-16 16:30:54 +0300

    chore(xm-private-ui): add RequestCacheFactoryService

--- src/app/shared/core/src/config/xm-private-ui-config.service.ts
+++ src/app/shared/core/src/config/xm-private-ui-config.service.ts
@@ -1,10 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, OnDestroy } from '@angular/core';
 import { XmPermissionService } from '@xm-ngx/core/permission';
-import { takeUntilOnDestroy } from '@xm-ngx/shared/operators';
-import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { switchMap, take } from 'rxjs/operators';
 import { RequestCache } from '../cache/request-cache';
+import { RequestCacheFactoryService } from '../cache/request-cache-factory.service';
 import { XmCoreConfig } from '../xm-core-config';
 import { UIPrivateConfig } from './xm-ui-config-model';
 
@@ -12,22 +12,37 @@ import { UIPrivateConfig } from './xm-ui-config-model';
     providedIn: 'root',
 })
 export class XmPrivateUiConfigService<T = UIPrivateConfig> implements OnDestroy {
-    private requestCache: RequestCache<T> = new RequestCache<T>(() => this.privateAPI());
+    private requestCache: RequestCache<T>;
 
     constructor(private httpClient: HttpClient,
+                private cacheFactoryService: RequestCacheFactoryService,
                 private permissionService: XmPermissionService,
                 private xmCoreConfig: XmCoreConfig) {
+        this.requestCache = this.cacheFactoryService.create<T>({
+            request: () => this.resolveConfigAPI(),
+            onlyWithUserSession: true,
+        });
     }
 
     public get config$(): Observable<T | null> {
-        return this.permissionService.hasPrivilege(this.xmCoreConfig.UI_PRIVATE_CONFIG_PERMISSION).pipe(
-            takeUntilOnDestroy(this),
-            switchMap(() => this.requestCache.get()),
-        );
+        return this.requestCache.get();
     }
 
     public ngOnDestroy(): void {
         this.requestCache.ngOnDestroy();
+    }
+
+    private resolveConfigAPI(): Observable<T> {
+        return this.permissionService.hasPrivilege(this.xmCoreConfig.UI_PRIVATE_CONFIG_PERMISSION).pipe(
+            take(1),
+            switchMap((allow) => {
+                if (allow) {
+                    return this.privateAPI();
+                } else {
+                    return of(null);
+                }
+            }),
+        );
     }
 
     private privateAPI(): Observable<T> {
