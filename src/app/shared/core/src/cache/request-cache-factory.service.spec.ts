d34eab441a7136de0c7ee94ca84fb55006fae849
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2020-04-16 15:07:10 +0300

    feat(request-cache): add RequestCacheFactoryService

---
+++ src/app/shared/core/src/cache/request-cache-factory.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { RequestCacheFactoryService } from './request-cache-factory.service';
+
+describe('RequestCacheFactoryService', () => {
+  let service: RequestCacheFactoryService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(RequestCacheFactoryService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
