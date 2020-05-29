44c847035c92af313878ac70436c874fae669e2f
vklimenok <vladyslav.klymenok@jevera.software>
2020-05-29 11:31:44 +0300

    Add autoversioning

---
+++ scripts/version.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export version=$(npm --silent run-script get-version | tail -n 1)
+IFS='.'
+read -ra versionArr <<< "$version"
+
+export majorVersion=${versionArr[0]}
+export minorVersion=${versionArr[1]}
+export patchVersion=${versionArr[2]}
+
+patchVersion=$((patchVersion - 1))
+npm --silent version $majorVersion.$minorVersion.$patchVersion --force --no-git-tag-version
+echo "The version was decremented successfully:" $majorVersion.$minorVersion.$patchVersion
