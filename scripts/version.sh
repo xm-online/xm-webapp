c22b6e4cbc013499d13cac560a5bc918d2d0ab12
vklimenok <vladyslav.klymenok@jevera.software>
2020-05-29 12:29:16 +0300

    Update script

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-export version=$(npm --silent run-script get-version)
+version=$(npm --silent run-script get-version)
 IFS='.'
 read -ra versionArr <<< "$version"
 
-export majorVersion=${versionArr[0]}
-export minorVersion=${versionArr[1]}
-export patchVersion=${versionArr[2]}
+majorVersion=${versionArr[0]}
+minorVersion=${versionArr[1]}
+patchVersion=${versionArr[2]}
 
 patchVersion=$((patchVersion - 1))
 npm --silent version $majorVersion.$minorVersion.$patchVersion --force --no-git-tag-version
