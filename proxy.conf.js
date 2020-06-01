72379a9b82378cc961f37469b4ac9565d8420a1c
ssenko <sergeysenja1992@gmail.com>
2020-06-01 22:38:51 +0300

    add dialog

--- proxy.conf.js
+++ proxy.conf.js
@@ -43,7 +43,7 @@ const PROXY_CONFIG = [
             "/zendesk",
             "/zendesk/management"
         ],
-        target: "http://xm.test.xm-online.com.ua",
+        target: "https://compass-int.cranecompass.com",
         secure: false,
         changeOrigin: true,
         logLevel: "debug"
