6557c6721e4cf3a306b1e97d2bf73ab5fd36732f
ozelenyi <oleksandr.zelenyi@jevera.software>
2020-06-25 16:09:45 +0300

    fix(core): revert unwanted changes

--- src/app/ext-commons/ext-common-entity/location-map-widget/location-map-widget.component.ts
+++ src/app/ext-commons/ext-common-entity/location-map-widget/location-map-widget.component.ts
@@ -1,11 +1,10 @@
 import { HttpResponse } from '@angular/common/http';
-import {Component, NgZone, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { filter } from 'rxjs/operators';
 
 import { buildMapId, buildPinSymbol } from '../../../shared/helpers/google-map-helper';
 import { XmEntity, XmEntityService } from '../../../xm-entity/';
-import {MarkerManager} from '@agm/core';
 
 declare const google: any;
 declare const MarkerClusterer: any;
@@ -24,10 +23,8 @@ export class LocationMapWidgetComponent implements OnInit {
     public currentGroup: any;
     public markerClusterer: any;
     public gMapApiReady$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-    public loading$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
-    constructor(private xmEntityService: XmEntityService,
-                private ngZone: NgZone) {
+    constructor(private xmEntityService: XmEntityService) {
     }
 
     public ngOnInit(): void {
@@ -35,7 +32,6 @@ export class LocationMapWidgetComponent implements OnInit {
         this.name = this.config.name;
         this.groups = this.config.groups;
         this.currentGroup = this.groups[0];
-        console.log('onInit', this);
         this.initMap();
     }
 
@@ -45,8 +41,6 @@ export class LocationMapWidgetComponent implements OnInit {
     }
 
     public initMap(): void {
-        console.log('initMap', this);
-        this.loading$.next(true);
         this.xmEntityService.search({
             query: this.currentGroup.query,
             size: this.currentGroup.size ? this.currentGroup.size : 20,
@@ -54,25 +48,19 @@ export class LocationMapWidgetComponent implements OnInit {
             (res: HttpResponse<XmEntity[]>) => {
                 this.gMapApiReady$
                     .pipe(
-                        filter((status) => status),
+                        filter((status) => status === true),
                     )
-                    .subscribe(() => {
-                        this.loading$.next(false);
-                        this.onShowMap(res.body);
-
-                    });
+                    .subscribe(() => this.onShowMap(res.body));
             },
             () => console.warn('Error'),
         );
     }
 
     public onAfterGMapApiInit(): void {
-        console.log('onAfterGMapApiInit', this);
         this.gMapApiReady$.next(true);
     }
 
     public onShowMap(data: any[]): void {
-        console.log('onShowMap', this);
         if (this.markerClusterer) {
             this.markerClusterer.clearMarkers();
         }
@@ -80,44 +68,38 @@ export class LocationMapWidgetComponent implements OnInit {
             scrollwheel: false,
         };
 
-
         const bounds = new google.maps.LatLngBounds();
         const map = new google.maps.Map(document.getElementById(this.mapId), mapOptions);
         const markers = [];
-        const mgr = new MarkerManager(map, this.ngZone);
-        this.markerClusterer = new MarkerClusterer(map, markers, {
-            imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m',
-        });
         for (const xmEntity of data) {
             const locations: any[] = xmEntity.locations;
 
-            locations.forEach((location, index) => {
+            for (const location of locations) {
                 if ((location.latitude > -90 && location.latitude < 90)
                     && (location.longitude > -180 && location.longitude < 180)) {
                     // TODO: it should be filter by location type based on widget config
-                        const loc = new google.maps.LatLng(location.latitude, location.longitude);
-                        const marker = new google.maps.Marker({
-                            position: loc,
-                            icon: buildPinSymbol(this.currentGroup.color),
-                        });
+                    const loc = new google.maps.LatLng(location.latitude, location.longitude);
+                    const marker = new google.maps.Marker({
+                        position: loc,
+                        icon: buildPinSymbol(this.currentGroup.color),
+                    });
 
-                        markers.push(marker);
-                        this.markerClusterer.addMarker(marker);
-                        mgr.addMarker(marker);
-                        marker.setMap(map);
-                        bounds.extend(loc);
+                    markers.push(marker);
+                    marker.setMap(map);
+                    bounds.extend(loc);
 
-                        // const infowindow = new google.maps.InfoWindow({
-                        //     content: xmEntity.name,
-                        // });
-                        // marker.addListener('click', () => infowindow.open(map, marker));
-
+                    const infowindow = new google.maps.InfoWindow({
+                        content: xmEntity.name,
+                    });
+                    marker.addListener('click', () => infowindow.open(map, marker));
                 }
-            })
+            }
         }
-        // map.fitBounds(bounds);
+        this.markerClusterer = new MarkerClusterer(map, markers, {
+            imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m',
+        });
+        map.fitBounds(bounds);
         map.panToBounds(bounds);
-        console.log('showMap End');
     }
 
 }
