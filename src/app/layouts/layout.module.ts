58036d4bd7f4fd4b3c2110b8c272258d0719b4fc
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2020-07-02 11:49:27 +0300

    chore(navbar): split to components

--- src/app/layouts/layout.module.ts
+++ src/app/layouts/layout.module.ts
@@ -3,21 +3,21 @@ import { RouterModule } from '@angular/router';
 import { FeedbackModule } from '@xm-ngx/components/feedback';
 import { LanguageModule } from '@xm-ngx/components/language';
 import { LoaderModule } from '@xm-ngx/components/loader';
+import { XmNotificationsModule } from '@xm-ngx/components/xm-notifications';
 import { XmPasswordNeededModule } from '@xm-ngx/components/xm-password-needed';
+import { XmRibbonModule } from '@xm-ngx/components/xm-ribbon';
 import { XmDynamicModule } from '@xm-ngx/dynamic';
 import { XmEntityModule } from '@xm-ngx/entity';
 import { XmSharedModule } from '@xm-ngx/shared';
 import { XmBalanceModule } from '@xm-ngx/xm-balance';
-import { XmRibbonModule } from '@xm-ngx/components/xm-ribbon';
 import { XmSidebarModule } from '../modules/xm-sidebar';
 import { XmSidebarRightModule } from '../modules/xm-sidebar-right';
 import { XmMaintenanceViewModule } from '../shared/components/maintenance/xm-maintenance-view.module';
 import { InputModule } from '../shared/directives/input.module';
-import { XmNotificationsModule } from '@xm-ngx/components/xm-notifications';
 import { XmTimelineModule } from '../xm-timeline/xm-timeline.module';
 import { FooterComponent } from './footer/footer.component';
 import { XmMainComponent } from './main/main.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { XmNavbarModule } from './navbar/xm-navbar.module';
 import { PageRibbonComponent } from './profiles/page-ribbon.component';
 
 @NgModule({
@@ -38,11 +38,11 @@ import { PageRibbonComponent } from './profiles/page-ribbon.component';
         XmSharedModule,
         XmDynamicModule,
         FeedbackModule,
+        XmNavbarModule,
     ],
     exports: [XmMainComponent],
     declarations: [
         XmMainComponent,
-        NavbarComponent,
         PageRibbonComponent,
         FooterComponent,
     ],
