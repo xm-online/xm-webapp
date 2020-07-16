602de155cad5963805e194d1db0efaf65959d980
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2020-07-16 18:13:05 +0300

    fix(xm-user): add permissions

--- packages/xm-account/src/xm-user-password-widget/xm-user-password-widget.module.ts
+++ packages/xm-account/src/xm-user-password-widget/xm-user-password-widget.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule, Type } from '@angular/core';
+import { XmPermissionModule } from '@xm-ngx/core/permission';
 import { PasswordModule } from '../../../../src/app/account/password/password.module';
 import { XmUserPasswordWidgetComponent } from './xm-user-password-widget.component';
 
 @NgModule({
     declarations: [XmUserPasswordWidgetComponent],
     exports: [XmUserPasswordWidgetComponent],
-    imports: [CommonModule, PasswordModule],
+    imports: [CommonModule, PasswordModule, XmPermissionModule],
 })
 export class XmUserPasswordWidgetModule {
     public entry: Type<XmUserPasswordWidgetComponent> = XmUserPasswordWidgetComponent;
