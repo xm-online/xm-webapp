442e6f0775e47f689a97b0d89ccb82f3c6882047
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2020-07-28 15:23:16 +0300

    fix(dashboards): fix caches

--- packages/components/src/xm-notifications/xm-notifications.module.ts
+++ packages/components/src/xm-notifications/xm-notifications.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule, Type } from '@angular/core';
+import { XmPermissionModule } from '@xm-ngx/core/permission';
 
 import { NotificationsComponent } from './notifications/notifications.component';
 import { NotificationsService } from './shared/notifications.service';
@@ -7,16 +8,11 @@ import { NotificationsService } from './shared/notifications.service';
 @NgModule({
     imports: [
         CommonModule,
+        XmPermissionModule,
     ],
-    declarations: [
-        NotificationsComponent,
-    ],
-    exports: [
-        NotificationsComponent,
-    ],
-    providers: [
-        NotificationsService,
-    ],
+    declarations: [NotificationsComponent],
+    exports: [NotificationsComponent],
+    providers: [NotificationsService],
 })
 export class XmNotificationsModule {
     public entry: Type<NotificationsComponent> = NotificationsComponent;
