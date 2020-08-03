4ad1d135f37cff8ee00a8c28833681cbafd3acd1
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2020-08-03 10:59:36 +0300

    chore(navbar): decompose (#603)
    * chore(navbar): decompose
    
    * chore(navbar-search): styles

--- packages/components/src/xm-notifications/xm-notifications.module.ts
+++ packages/components/src/xm-notifications/xm-notifications.module.ts
@@ -1,15 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule, Type } from '@angular/core';
 import { XmPermissionModule } from '@xm-ngx/core/permission';
+import { XmTranslationModule } from '@xm-ngx/translation';
 
 import { NotificationsComponent } from './notifications/notifications.component';
 import { NotificationsService } from './shared/notifications.service';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        XmPermissionModule,
-    ],
+  imports: [
+    CommonModule,
+    XmPermissionModule,
+    XmTranslationModule,
+  ],
     declarations: [NotificationsComponent],
     exports: [NotificationsComponent],
     providers: [NotificationsService],
