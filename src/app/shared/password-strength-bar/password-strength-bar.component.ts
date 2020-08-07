b2771b58b5668877f0bcbfc92b0ac4f92b8145c1
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2020-08-07 15:47:49 +0300

    chore(translate): switch to translate pipe (#636)

--- src/app/shared/password-strength-bar/password-strength-bar.component.ts
+++ src/app/shared/password-strength-bar/password-strength-bar.component.ts
@@ -4,7 +4,7 @@ import { Component, ElementRef, Input, Renderer2 } from '@angular/core';
     selector: 'xm-password-strength-bar',
     template: `
         <div id="strength">
-            <small jhiTranslate="global.messages.validate.newpassword.strength">Password strength:</small>
+            <small>{{'global.messages.validate.newpassword.strength'|translate}}</small>
             <ul id="strengthBar">
                 <li class="point"></li>
                 <li class="point"></li>
