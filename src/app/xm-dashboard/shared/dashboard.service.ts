e0f70a144324550a96b410c45f84d47ff638090a
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2020-08-08 16:46:37 +0300

    fix(dashboards): add catchError
    fix sidebar show logout by default

--- src/app/xm-dashboard/shared/dashboard.service.ts
+++ src/app/xm-dashboard/shared/dashboard.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SKIP_ERROR_HANDLER_INTERCEPTOR_HEADERS } from '@xm-ngx/core';
 import { createRequestOption } from '@xm-ngx/entity';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -7,7 +8,7 @@ import { map } from 'rxjs/operators';
 import { SERVER_API_URL } from '../../xm.constants';
 import { Dashboard, DashboardWithWidgets } from './dashboard.model';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class DashboardService {
 
     private resourceUrl: string = `${SERVER_API_URL}/dashboard/api/dashboards`;
@@ -17,34 +18,41 @@ export class DashboardService {
 
     public create(dashboard: Dashboard): Observable<HttpResponse<Dashboard>> {
         const copy = this.convert(dashboard);
-        return this.http.post<Dashboard>(this.resourceUrl, copy, {observe: 'response'}).pipe(
+        return this.http.post<Dashboard>(this.resourceUrl, copy, { observe: 'response' }).pipe(
             map((res: HttpResponse<Dashboard>) => this.convertResponse(res)));
     }
 
     public update(dashboard: Dashboard): Observable<HttpResponse<Dashboard>> {
         const copy = this.convert(dashboard);
-        return this.http.put<Dashboard>(this.resourceUrl, copy, {observe: 'response'}).pipe(
+        return this.http.put<Dashboard>(this.resourceUrl, copy, { observe: 'response' }).pipe(
             map((res: HttpResponse<Dashboard>) => this.convertResponse(res)));
     }
 
     public find(id: number): Observable<HttpResponse<DashboardWithWidgets>> {
-        return this.http.get<Dashboard>(`${this.resourceUrl}/${id}`, {observe: 'response'}).pipe(
+        return this.http.get<Dashboard>(`${this.resourceUrl}/${id}`, { observe: 'response' }).pipe(
             map((res: HttpResponse<Dashboard>) => this.convertResponse(res)));
     }
 
     public query(req?: any): Observable<HttpResponse<Dashboard[]>> {
         const options = createRequestOption(req);
-        return this.http.get<Dashboard[]>(this.resourceUrl, {params: options, observe: 'response'}).pipe(
+        return this.http.get<Dashboard[]>(
+            this.resourceUrl,
+            {
+                params: options,
+                headers: SKIP_ERROR_HANDLER_INTERCEPTOR_HEADERS,
+                observe: 'response',
+            },
+        ).pipe(
             map((res: HttpResponse<Dashboard[]>) => this.convertArrayResponse(res)));
     }
 
     public delete(id: number): Observable<HttpResponse<any>> {
-        return this.http.delete<any>(`${this.resourceUrl}/${id}`, {observe: 'response'});
+        return this.http.delete<any>(`${this.resourceUrl}/${id}`, { observe: 'response' });
     }
 
     private convertResponse(res: HttpResponse<Dashboard>): HttpResponse<Dashboard> {
         const body: Dashboard = this.convertItemFromServer(res.body);
-        return res.clone({body});
+        return res.clone({ body });
     }
 
     private convertArrayResponse(res: HttpResponse<Dashboard[]>): HttpResponse<Dashboard[]> {
@@ -54,20 +62,20 @@ export class DashboardService {
         for (const i of jsonResponse) {
             body.push(this.convertItemFromServer(i));
         }
-        return res.clone({body});
+        return res.clone({ body });
     }
 
     /**
      * Convert a returned JSON object to Dashboard.
      */
     private convertItemFromServer(dashboard: Dashboard): Dashboard {
-        return {...dashboard};
+        return { ...dashboard };
     }
 
     /**
      * Convert a Dashboard to a JSON which can be sent to the server.
      */
     private convert(dashboard: Dashboard): Dashboard {
-        return {...dashboard};
+        return { ...dashboard };
     }
 }
