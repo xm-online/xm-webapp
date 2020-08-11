8c1057f773c426a786c25bd4a7b35deac649559d
Andrew Kikot <andrew.kikot@gmail.com>
2020-08-11 17:42:44 +0300

    refactor(md-editor): unsubscribe, mat-buttons, typings

--- src/app/ext-commons/ext-common/md-widget/md-widget.component.ts
+++ src/app/ext-commons/ext-common/md-widget/md-widget.component.ts
@@ -1,21 +1,31 @@
 import { HttpResponse } from '@angular/common/http';
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, ViewChild } from '@angular/core';
 
 import { Widget, WidgetService } from '@xm-ngx/dashboard';
 import { TdTextEditorComponent } from '@covalent/text-editor';
+import { ITranslate } from '@xm-ngx/translation';
+import { takeUntilOnDestroy, takeUntilOnDestroyDestroy } from '@xm-ngx/shared/operators';
+
+export interface MdEditorOptions {
+    autofocus?: boolean;
+    status?: boolean;
+    promptURLs?: boolean;
+    spellChecker?: boolean;
+    showIcons?: string[];
+}
 
 @Component({
     selector: 'xm-md-widget',
     templateUrl: './md-widget.component.html',
     styleUrls: ['./md-widget.component.scss'],
 })
-export class MdWidgetComponent implements AfterViewInit {
+export class MdWidgetComponent implements AfterViewInit, OnDestroy {
 
-    public config: any;
-    public widgetConfig: any;
+    public config: { id: number, name: string | ITranslate };
+    public widgetConfig: unknown;
     public isEditMode: boolean;
-    public editorOptions: any = {
-        autofocus: true,
+    public editorOptions: MdEditorOptions = {
+        autofocus: false,
         status: true,
         promptURLs: true,
         spellChecker: false,
@@ -27,15 +37,22 @@ export class MdWidgetComponent implements AfterViewInit {
     }
 
     public ngAfterViewInit(): void {
-        this.widgetService.find(this.config.id).subscribe((result: HttpResponse<Widget>) => {
-            const w: Widget = result && result.body;
-            this.widgetConfig = w.config || null;
-            setTimeout(() => {
-                if (!this._textEditor.isPreviewActive()) {
-                    this._textEditor.togglePreview();
-                }
-            }, 0);
-        });
+        this.widgetService
+            .find(this.config.id)
+            .pipe(takeUntilOnDestroy(this))
+            .subscribe((result: HttpResponse<Widget>) => {
+                const w: Widget = result && result.body;
+                this.widgetConfig = w.config || null;
+                setTimeout(() => {
+                    if (!this._textEditor.isPreviewActive()) {
+                        this._textEditor.togglePreview();
+                    }
+                }, 0);
+            });
+    }
+
+    public ngOnDestroy(): void {
+        takeUntilOnDestroyDestroy(this);
     }
 
     public onEditMode(): void {
@@ -44,14 +61,16 @@ export class MdWidgetComponent implements AfterViewInit {
     }
 
     public onSave(): void {
-        this.widgetService.find(this.config.id).subscribe((result: HttpResponse<Widget>) => {
+        this.widgetService
+            .find(this.config.id)
+            .pipe(takeUntilOnDestroy(this))
+            .subscribe((result: HttpResponse<Widget>) => {
             const widget: Widget = result.body;
             widget.config = widget.config || {};
             Object.assign(widget.config, {content: this._textEditor.value});
             Object.assign(widget, {dashboard: {id: Number(widget.dashboard)}}); // widget.dashboard must be an object
-            this.widgetService.update(widget).subscribe(() => {
+            this.widgetService.update(widget).pipe(takeUntilOnDestroy(this)).subscribe(() => {
                 this.isEditMode = false;
-                this.widgetConfig = {...widget.config};
                 if (!this._textEditor.isPreviewActive()) {
                     this._textEditor.togglePreview();
                 }
