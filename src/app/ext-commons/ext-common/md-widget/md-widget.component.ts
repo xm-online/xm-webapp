5f6b2d215b4817003ae714466dff588d68a7c5a4
andrewkikot <andrew.kikot@gmail.com>
2020-08-12 16:06:20 +0300

    fix(md-editor): fix editor full screen mode overlay (#648)
    * fix(md-editor): fix editor full screen mode overlay
    
    * refactor(md-widget): move to @covalent/text-editor
    
    * refactor(md-editor): unsubscribe, mat-buttons, typings
    
    * refactor(md-editor): types
    
    * refactor(md-editor): cleanup
    
    * refactor(md-editor): types

--- src/app/ext-commons/ext-common/md-widget/md-widget.component.ts
+++ src/app/ext-commons/ext-common/md-widget/md-widget.component.ts
@@ -1,50 +1,84 @@
 import { HttpResponse } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, ViewChild } from '@angular/core';
 
-import {Widget, WidgetService} from '@xm-ngx/dashboard';
+import { Widget, WidgetService } from '@xm-ngx/dashboard';
+import { TdTextEditorComponent } from '@covalent/text-editor';
+import { ITranslate } from '@xm-ngx/translation';
+import { takeUntilOnDestroy, takeUntilOnDestroyDestroy } from '@xm-ngx/shared/operators';
 
-declare let $: any;
-declare let SimpleMDE: any;
+export interface MdEditorOptions {
+    autofocus?: boolean;
+    status?: boolean;
+    promptURLs?: boolean;
+    spellChecker?: boolean;
+    showIcons?: string[];
+}
+
+export interface MdWidgetConfig {
+    contenct: string;
+    content: string;
+    isEditable: boolean;
+}
 
 @Component({
     selector: 'xm-md-widget',
     templateUrl: './md-widget.component.html',
     styleUrls: ['./md-widget.component.scss'],
 })
-export class MdWidgetComponent implements OnInit {
+export class MdWidgetComponent implements AfterViewInit, OnDestroy {
 
-    public name: any;
-    public config: any;
-    public mdEditor: any;
+    public config: { id: number, name: string | ITranslate };
+    public widgetConfig: MdWidgetConfig;
     public isEditMode: boolean;
+    public editorOptions: MdEditorOptions = {
+        autofocus: false,
+        status: true,
+        promptURLs: true,
+        spellChecker: false,
+        showIcons: ['code', 'table'],
+    };
+    @ViewChild('mdEditor', {static: false}) private _textEditor: TdTextEditorComponent;
 
     constructor(private widgetService: WidgetService) {
     }
 
-    public ngOnInit(): void {
-        this.name = this.config.name;
-        this.mdEditor = new SimpleMDE({
-            element: $('#xm-widget-md')[0],
-        });
-        this.mdEditor.value(this.config.content);
-        this.mdEditor.togglePreview();
+    public ngAfterViewInit(): void {
+        this.widgetService
+            .find(this.config.id)
+            .pipe(takeUntilOnDestroy(this))
+            .subscribe((result: HttpResponse<Widget>) => {
+                const w: Widget = result && result.body;
+                this.widgetConfig = w.config || null;
+                setTimeout(() => {
+                    if (!this._textEditor.isPreviewActive()) {
+                        this._textEditor.togglePreview();
+                    }
+                }, 0);
+            });
+    }
+
+    public ngOnDestroy(): void {
+        takeUntilOnDestroyDestroy(this);
     }
 
     public onEditMode(): void {
         this.isEditMode = !this.isEditMode;
-        this.mdEditor.togglePreview();
+        this._textEditor.togglePreview();
     }
 
     public onSave(): void {
-        this.widgetService.find(this.config.id).subscribe((result: HttpResponse<Widget>) => {
+        this.widgetService
+            .find(this.config.id)
+            .pipe(takeUntilOnDestroy(this))
+            .subscribe((result: HttpResponse<Widget>) => {
             const widget: Widget = result.body;
             widget.config = widget.config || {};
-            Object.assign(widget.config, {content: this.mdEditor.value()});
+            Object.assign(widget.config, {content: this._textEditor.value});
             Object.assign(widget, {dashboard: {id: Number(widget.dashboard)}}); // widget.dashboard must be an object
-            this.widgetService.update(widget).subscribe(() => {
+            this.widgetService.update(widget).pipe(takeUntilOnDestroy(this)).subscribe(() => {
                 this.isEditMode = false;
-                if (!this.mdEditor.isPreviewActive()) {
-                    this.mdEditor.togglePreview();
+                if (!this._textEditor.isPreviewActive()) {
+                    this._textEditor.togglePreview();
                 }
             });
         });
