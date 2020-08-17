9ccbfa702a6726084f4db10e687ee3b946020443
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2020-08-17 10:13:09 +0300

    feat(xm-mat-card): add xm-mat-card

---
+++ src/app/xm-entity/xm-mat-card/xm-mat-card.component.ts
@@ -0,0 +1,50 @@
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { matExpansionAnimations } from '@angular/material/expansion';
+import { IId } from '@xm-ngx/components/entity-collection';
+import { JavascriptCode } from '@xm-ngx/shared/interfaces/javascript-code';
+import { Translate } from '@xm-ngx/translation';
+
+export interface XmMatCardOptions {
+    editCondition: JavascriptCode;
+    title: Translate;
+    readonly: boolean;
+    condition: JavascriptCode;
+    collapsableContent?: boolean;
+    contentHiddenByDefault?: boolean;
+}
+
+@Component({
+    selector: 'xm-mat-card',
+    templateUrl: './xm-mat-card.component.html',
+    styleUrls: ['./xm-mat-card.component.scss'],
+    animations: [
+        matExpansionAnimations.bodyExpansion,
+        matExpansionAnimations.indicatorRotate,
+    ],
+})
+export class XmMatCardComponent implements OnInit, OnChanges {
+
+    @Input() public options: XmMatCardOptions;
+    @Input() public entity: IId;
+    @Output() public save: EventEmitter<void> = new EventEmitter<void>();
+    @Output() public cancel: EventEmitter<void> = new EventEmitter<void>();
+    @Output() public isEditChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+    public isEdit: boolean = false;
+    public contentHidden: boolean;
+
+    public ngOnInit(): void {
+        this.contentHidden = this.options.contentHiddenByDefault;
+        this.updateEditStateByEntityId();
+    }
+
+    public ngOnChanges(changes: SimpleChanges): void {
+        if (changes.entity) {
+            this.updateEditStateByEntityId();
+        }
+    }
+
+    public updateEditStateByEntityId(): void {
+        this.isEdit = !this.entity?.id;
+    }
+}
