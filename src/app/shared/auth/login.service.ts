cf431be02c50b7832329bc948548b140a81b5a84
ochuhaiev <ochuhaiev@itsmartflex.com>
2020-08-25 14:04:09 +0300

    fix(auth): apply user locale after identity

--- src/app/shared/auth/login.service.ts
+++ src/app/shared/auth/login.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-
-import { JhiLanguageService } from 'ng-jhipster';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { AuthServerProvider } from './auth-jwt.service';
@@ -11,8 +9,7 @@ import { StateStorageService } from './state-storage.service';
 @Injectable()
 export class LoginService {
 
-    constructor(private jhiLanguageService: JhiLanguageService,
-                private principal: Principal,
+    constructor(private principal: Principal,
                 private router: Router,
                 private authServerProvider: AuthServerProvider,
                 private stateStorageService: StateStorageService) {
@@ -65,13 +62,6 @@ export class LoginService {
 
     private getUserIdentity(next: any, data: any): void {
         this.principal.identity(true, true).then((account) => {
-            /*
-             * After the login the language will be changed to
-             * the language selected by the user during his registration
-             */
-            if (account !== null && account.langKey) {
-                this.jhiLanguageService.changeLanguage(account.langKey);
-            }
             if (next) {
                 next(data);
             }
