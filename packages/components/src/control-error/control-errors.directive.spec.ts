4b21cba7d142f48b0f07cd9f59303bb679d3bd68
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2020-08-26 12:09:40 +0300

    test(page-collection): fix config null (#700)

--- packages/components/src/control-error/control-errors.directive.spec.ts
+++ packages/components/src/control-error/control-errors.directive.spec.ts
@@ -2,7 +2,7 @@ import { ControlErrorsDirective } from './control-errors.directive';
 
 describe('ControlErrorsDirective', () => {
     it('should create an instance', () => {
-        const directive = new ControlErrorsDirective(null, null, null);
+        const directive = new ControlErrorsDirective(null, null, null, null);
         expect(directive).toBeTruthy();
     });
 });
--- packages/components/src/navbar-guest-background/navbar-guest-background.directive.spec.ts
+++ packages/components/src/navbar-guest-background/navbar-guest-background.directive.spec.ts
@@ -1,8 +1,8 @@
 import { NavbarGuestBackgroundDirective } from './navbar-guest-background.directive';
 
 describe('NavbarGuestBackgroundDirective', () => {
-  it('should create an instance', () => {
-    const directive = new NavbarGuestBackgroundDirective();
-    expect(directive).toBeTruthy();
-  });
+    it('should create an instance', () => {
+        const directive = new NavbarGuestBackgroundDirective(null, null);
+        expect(directive).toBeTruthy();
+    });
 });
