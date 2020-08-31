10c810a09e891cb07b685757c187f833e0fa384b
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2020-08-31 10:39:09 +0300

    feat(sidebar-user): add subtitles (#710)

--- src/app/modules/xm-sidebar/sidebar.component.ts
+++ src/app/modules/xm-sidebar/sidebar.component.ts
@@ -1,4 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { XmUiConfigService } from '@xm-ngx/core';
+import { takeUntilOnDestroy, takeUntilOnDestroyDestroy } from '@xm-ngx/shared/operators';
+import { get } from 'lodash';
 
 @Component({
     selector: 'xm-sidebar',
@@ -10,5 +13,20 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
     changeDetection: ChangeDetectionStrategy.Default,
 })
 export class SidebarComponent {
+    public config: { user: unknown };
 
+    constructor(
+        private xmConfigService: XmUiConfigService<{sidebar: { user: unknown }}>,
+    ) {
+    }
+
+    public ngOnInit(): void {
+        this.xmConfigService.config$().pipe(takeUntilOnDestroy(this)).subscribe(
+            (config) => this.config = get(config, 'sidebar'),
+        );
+    }
+
+    public ngOnDestroy(): void {
+        takeUntilOnDestroyDestroy(this);
+    }
 }
