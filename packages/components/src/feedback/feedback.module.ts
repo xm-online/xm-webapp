6e361a6f677149016a8750690a6051b134f294ea
ykrasovskyi <eugenekrasovskiy@gmail.com>
2020-09-09 17:19:11 +0300

    feat(b2bcrm-479): add message after sent feedback

--- packages/components/src/feedback/feedback.module.ts
+++ packages/components/src/feedback/feedback.module.ts
@@ -6,14 +6,16 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { ModalCloseModule } from '@xm-ngx/components/modal-close';
 import { XmTranslationModule } from '@xm-ngx/translation';
 import { FeedbackDialogComponent } from './feedback-dialog/feedback-dialog.component';
 import { FeedbackComponent } from './feedback.component';
-import { ModalCloseModule } from '@xm-ngx/components/modal-close';
 
 @NgModule({
     imports: [
         MatDialogModule,
+        MatTooltipModule,
         XmTranslationModule,
         FormsModule,
         MatFormFieldModule,
