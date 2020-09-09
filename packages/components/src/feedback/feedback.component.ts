1527c299a1c55983ec898b95d52e4b7366391eff
ykrasovskyi <eugenekrasovskiy@gmail.com>
2020-09-09 17:19:11 +0300

    feat(b2bcrm-479): add message after sent feedback

--- packages/components/src/feedback/feedback.component.ts
+++ packages/components/src/feedback/feedback.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { XmToasterService } from "@xm-ngx/toaster";
 import { filter, switchMap } from 'rxjs/operators';
 import { FeedbackDialogComponent } from './feedback-dialog/feedback-dialog.component';
 import { FeedbackService } from './feedback.service';
@@ -13,7 +14,10 @@ export interface FeedbackConfig {
 @Component({
     selector: 'xm-feedback',
     template: `
-        <button *ngIf="config" (click)="create(config.feedback.url)" mat-icon-button>
+        <button *ngIf="config"
+                (click)="create(config.feedback.url)"
+                [matTooltip]="'dashboard-config-widget.feedback.tooltip' | translate"
+                mat-icon-button>
             <mat-icon>feedback</mat-icon>
         </button>
     `,
@@ -25,6 +29,7 @@ export class FeedbackComponent {
     constructor(
         protected dialog: MatDialog,
         protected feedbackService: FeedbackService,
+        private toasterService: XmToasterService
     ) {
     }
 
@@ -36,6 +41,14 @@ export class FeedbackComponent {
         dialogRef.afterClosed().pipe(
             filter((i) => i),
             switchMap(res => this.feedbackService.create(res, url)),
-        ).subscribe();
+        ).subscribe(
+            () => this.toasterService.create({
+                type: 'success',
+                text: 'dashboard-config-widget.feedback.success'
+            }).subscribe(),
+            () => this.toasterService.create({
+                type: 'info',
+                text: 'dashboard-config-widget.feedback.error'
+            }).subscribe());
     }
 }
