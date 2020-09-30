6057dec45089b8ec46eb1b1544a7cb4baccf90cd
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2020-09-30 10:27:09 +0300

    chore(core-auth): rename from AuthExpiredInterceptor to AuthInterceptor

--- packages/core/src/auth/src/xm-core-auth.module.ts
+++ packages/core/src/auth/src/xm-core-auth.module.ts
@@ -1,6 +1,6 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Injector, ModuleWithProviders, NgModule } from '@angular/core';
-import { AuthExpiredInterceptor } from './auth-expired.interceptor';
+import { AuthInterceptor } from './auth.interceptor';
 
 @NgModule({
     imports: [],
@@ -14,7 +14,7 @@ export class XmCoreAuthModule {
             providers: [
                 {
                     provide: HTTP_INTERCEPTORS,
-                    useClass: AuthExpiredInterceptor,
+                    useClass: AuthInterceptor,
                     multi: true,
                     deps: [Injector],
                 },
