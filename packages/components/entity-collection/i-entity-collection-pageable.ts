45e6509d6f758582d072f85a25fc73f09112d433
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2020-10-11 15:02:08 +0300

    chore(entity-collection): add sortable

--- packages/components/entity-collection/i-entity-collection-pageable.ts
+++ packages/components/entity-collection/i-entity-collection-pageable.ts
@@ -3,6 +3,13 @@ import { Id, IId } from '@xm-ngx/shared/interfaces';
 import { Observable } from 'rxjs';
 import { IEntityCollection, QueryParams } from './i-entity-collection';
 
+export interface Sortable {
+    /** Sort by the fields. */
+    by?: string[];
+    /** Sort order. */
+    order?: 'asc' | 'desc';
+}
+
 export interface Pageable {
     /** The <total> is the number of items that match the request. */
     total?: number;
@@ -12,7 +19,7 @@ export interface Pageable {
     size?: number;
 }
 
-export type QueryParamsPageable = QueryParams & Pageable;
+export type QueryParamsPageable = QueryParams & Pageable & Sortable;
 
 export interface IEntityCollectionPageable<T extends IId = unknown, Extra extends Pageable = Pageable>
     extends IEntityCollection<IId> {
--- packages/components/entity-collection/index.ts
+++ packages/components/entity-collection/index.ts
@@ -1,4 +1,5 @@
-export * from './entity-collection-factory.service';
-export * from './http-client.rest';
-export * from './i-entity-collection';
-export * from './entity-collection-base';
+export { EntityCollectionFactoryService } from './entity-collection-factory.service';
+export { HttpClientRest } from './http-client.rest';
+export { QueryParams, IEntityCollection } from './i-entity-collection';
+export { EntityCollectionBase } from './entity-collection-base';
+export { Pageable, Sortable, IEntityCollectionPageable, QueryParamsPageable } from './i-entity-collection-pageable';
