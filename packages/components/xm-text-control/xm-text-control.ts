ac4b69355c8f7518aebdc6dab35173b477e12064
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2020-10-11 14:01:15 +0300

    chore(text-control): add formControl

--- packages/components/xm-text-control/xm-text-control.ts
+++ packages/components/xm-text-control/xm-text-control.ts
@@ -11,15 +11,15 @@ import {
     Self,
     ViewEncapsulation,
 } from '@angular/core';
-import { FormsModule, NgControl } from '@angular/forms';
+import { FormControl, FormsModule, NgControl, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
+import { XM_CONTROL_ERRORS_TRANSLATES } from '@xm-ngx/components/control-error';
 import { ControlErrorModule } from '@xm-ngx/components/control-error/control-error.module';
 import { NgControlAccessor } from '@xm-ngx/components/ng-accessor';
 import { IControl, IControlFn } from '@xm-ngx/dynamic';
 import { Primitive } from '@xm-ngx/shared/interfaces';
 import { Translate, XmTranslationModule } from '@xm-ngx/translation';
 import { defaults } from 'lodash';
-import { XM_CONTROL_ERRORS_TRANSLATES } from "@xm-ngx/components/control-error";
 
 export interface ITextControlOptions {
     title?: string;
@@ -41,40 +41,43 @@ const DEFAULT_OPTIONS: ITextControlOptions = {
     id: null,
     name: 'text',
     required: true,
-    disabled: false
+    disabled: false,
 };
 
 @Component({
     selector: 'xm-text-control',
     template: `
         <mat-form-field>
-            <mat-label>{{options.title|translate}}</mat-label>
+            <mat-label>{{options.title | translate}}</mat-label>
 
             <input matInput
                    (ngModelChange)="change($event)"
-                   [(ngModel)]="value"
+                   [formControl]="formControl"
                    [placeholder]="options.placeholder | translate"
                    [disabled]="disabled || options.disabled"
                    [attr.name]="options.name"
                    [id]="options.id"
                    [required]="options.required"
                    [pattern]="options.pattern"
-                   #input="ngModel"
                    [attr.type]="options.type">
 
-            <mat-error *xmControlErrors="input?.errors || ngControl?.errors; translates options?.errors; message as message">{{message}}</mat-error>
+            <mat-error *xmControlErrors="formControl.errors; translates options?.errors; message as message">{{message}}</mat-error>
 
         </mat-form-field>
     `,
-    styles: [''],
     encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.Default,
 })
 export class XmTextControl extends NgControlAccessor<Primitive> implements IControl<Primitive, ITextControlOptions> {
     @Input() public value: Primitive;
     @Input() public disabled: boolean;
-
+    @Input() public formControl: FormControl = new FormControl();
     @Output() public valueChange: EventEmitter<Primitive> = new EventEmitter<Primitive>();
+
+    constructor(@Optional() @Self() public ngControl: NgControl | null,
+                @Inject(XM_CONTROL_ERRORS_TRANSLATES) private xmControlErrorsTranslates: { [errorKey: string]: Translate }) {
+        super(ngControl);
+    }
 
     private _options: ITextControlOptions = {};
 
@@ -84,13 +87,8 @@ export class XmTextControl extends NgControlAccessor<Primitive> implements ICont
 
     @Input()
     public set options(value: ITextControlOptions) {
-        this._options = defaults({}, value, {...DEFAULT_OPTIONS, errors: this.xmControlErrorsTranslates});
+        this._options = defaults({}, value, { ...DEFAULT_OPTIONS, errors: this.xmControlErrorsTranslates });
         this._options.placeholder = this._options.placeholder || this._options.title;
-    }
-
-    constructor(@Optional() @Self() public ngControl: NgControl | null,
-                @Inject(XM_CONTROL_ERRORS_TRANSLATES) private xmControlErrorsTranslates: { [errorKey: string]: Translate }) {
-        super(ngControl);
     }
 
 }
@@ -102,6 +100,7 @@ export class XmTextControl extends NgControlAccessor<Primitive> implements ICont
         XmTranslationModule,
         CommonModule,
         ControlErrorModule,
+        ReactiveFormsModule,
     ],
     exports: [XmTextControl],
     declarations: [XmTextControl],
