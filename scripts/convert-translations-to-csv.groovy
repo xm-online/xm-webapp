9dfb210d512cc4979c6d86c9a9c8b3eb7c797032
Vova Viter <Vova.viter@gmail.com>
2020-10-20 15:04:50 +0300

    Refactored to use OpenCsv. Added script to convert from csv to json

--- scripts/convert-translations-to-csv.groovy
+++ scripts/convert-translations-to-csv.groovy
@@ -1,44 +1,55 @@
+@Grab(group = 'net.sf.opencsv', module = 'opencsv', version = '2.3')
+import au.com.bytecode.opencsv.CSVWriter
 import groovy.io.FileType
-import groovy.json.*
-
+import groovy.json.JsonSlurper
 
 String inDir = this.args[0]
-String separator = this.args[2]
 String outDir = this.args[1]
 
-File dir  = new File(inDir)
+File dir = new File(inDir)
 
 List list = new LinkedList();
+println("Processing folder ${inDir}")
 dir.eachFileRecurse(FileType.FILES) { it ->
-  if(it.name.endsWith('.json')) {
+  if (it.name.endsWith('.json')) {
+    println("Started converting file ${it.name}")
     Map data = new JsonSlurper().parse(it)
     list.addAll(convert(data, "", it.name))
+    println("Finished converting file ${it.name}")
   }
 }
 
-PrintWriter writer = new PrintWriter(outDir.concat("/translations-${System.currentTimeMillis()}.csv"), "UTF-8")
-writer.println("key".concat(separator).concat("value").concat(separator).concat("fileName"))
-list.each{ it ->
-  writer.println("".concat(it.key).concat(separator).concat(it.value).concat(separator).concat(it.file))
+File csvFile = new File(outDir.concat("/translations-${System.currentTimeMillis()}.csv"))
+csvFile.createNewFile()
+
+csvFile.withWriter { writer ->
+  CSVWriter csvWriter = new CSVWriter(writer)
+  list.each { v ->
+    csvWriter.writeNext(v as String[])
+  }
 }
-writer.close()
+
+println("Output file created ${csvFile.path}")
+
 System.exit(0);
 
-List convert(Object value, String key, String fileName){
+List convert(Object value, String key, String fileName) {
   List retval = new LinkedList<>()
-  if (value instanceof Map){
-    value.each{ k, v ->
-      key1 = key.concat(".").concat(k.replace(".", "\$dot\$"))
-      retval.addAll(convert(v, key1, fileName))}
+  if (value instanceof Map) {
+    value.each { k, v ->
+      String nextKey = key.concat(key == "" ? "" : ".").concat(k.replace(".", "\$dot\$"))
+      retval.addAll(convert(v, nextKey, fileName))
+    }
   }
-  if (value instanceof List){
-    value.each{  v ->
-      retval.addAll(convert(v, key,fileName))}
+  if (value instanceof List) {
+    value.each { v ->
+      retval.addAll(convert(v, key, fileName))
+    }
   }
-  if (value instanceof String){
-    retval.add([key: key, value: value, file:fileName])
+  if (value instanceof String) {
+    retval.add([fileName, key, value])
   }
-  return  retval;
+  return retval;
 }
 
 
