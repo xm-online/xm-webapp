165f1525fb9594008072d52bb27ae2af0d2f5146
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2020-10-20 09:51:41 +0300

    chore(core): add key to session

--- packages/core/auth/src/user-session.service.ts
+++ packages/core/auth/src/user-session.service.ts
@@ -1,37 +1,48 @@
 import { Injectable } from '@angular/core';
 import { SessionService } from '@xm-ngx/core';
 import { Observable } from 'rxjs';
-import { distinctUntilChanged, filter, pluck } from 'rxjs/operators';
+import { distinctUntilChanged, filter, map, pluck, skip } from 'rxjs/operators';
 
 export interface IUserSession {
     active: boolean;
+    refreshToken: string;
+    accessToken: string;
+    useAutoRefreshToken: boolean;
 }
 
 /**
- * Modifing information about user session.
+ * Modifying information about user session.
  */
 @Injectable({ providedIn: 'root' })
 export class UserSessionService {
 
+    public dispose$: Observable<void> = this.isActive().pipe(
+        skip(1),
+        distinctUntilChanged((active) => active === false),
+        map(() => undefined),
+    );
+
+    public readonly sessionKey: string = 'UserSessionKey';
+
     constructor(
-        protected sessionService: SessionService<IUserSession>,
+        protected sessionService: SessionService,
     ) {
     }
 
-    public create(session: IUserSession = { active: true }): void {
-        this.sessionService.store(session);
+    public create(session: IUserSession): void {
+        this.sessionService.store(this.sessionKey, session);
     }
 
     public update(session: IUserSession): void {
-        this.sessionService.store(session);
+        this.sessionService.store(this.sessionKey, session);
     }
 
     public get(): Observable<IUserSession> {
-        return this.sessionService.get();
+        return this.sessionService.get(this.sessionKey);
     }
 
-    public destory(): void {
-        this.sessionService.store({ active: false });
+    public destroy(): void {
+        this.sessionService.store(this.sessionKey, { active: false });
     }
 
     public isActive(): Observable<boolean> {
