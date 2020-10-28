8b2556359e86466d95b5c88cafc7fd12d9b55191
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2020-10-28 14:02:32 +0200

    feat(transform-by-map): add multi level

---
+++ packages/shared/operators/src/transform-by-map.spec.ts
@@ -0,0 +1,35 @@
+import { transformByMap } from './transform-by-map';
+
+describe('transformByMap', () => {
+    it('should transform an one-level', () => {
+        const data = { oldName: 'Rex' };
+        const map = { newName: 'oldName' };
+        const expectation = { newName: 'Rex' };
+        const result = transformByMap(data, map);
+        expect(result).toEqual(expectation);
+    });
+
+    it('should transform multi-level', () => {
+        const data = { creds: { login: 'Rex' } };
+        const map = { 'credentials.nickname': 'creds.login' };
+        const expectation = { credentials: { nickname: 'Rex' } };
+        const result = transformByMap(data, map);
+        expect(result).toEqual(expectation);
+    });
+
+    it('should skip null data', () => {
+        const data = { creds: null };
+        const map = { 'credentials.nickname': 'creds.login' };
+        const expectation = {};
+        const result = transformByMap(data, map);
+        expect(result).toEqual(expectation);
+    });
+
+    it('should skip null sub-data', () => {
+        const data = { creds: { login: null } };
+        const map = { 'credentials.nickname': 'creds.login' };
+        const expectation = {};
+        const result = transformByMap(data, map);
+        expect(result).toEqual(expectation);
+    });
+});
--- packages/shared/operators/src/transform-by-map.ts
+++ packages/shared/operators/src/transform-by-map.ts
@@ -1,24 +1,30 @@
-import { get } from 'lodash';
+import { get, set } from 'lodash';
 
 /**
- * Transform object according to options.map
- * @param value - previous value
- * @param optionsMap
+ * Transform object according to options
+ * @param data - previous value
+ * @param mapper - where key is the new key and value is the key from the first argument
  * @return new value
  * @example
- *  entityMap({name: 'Rex'}, {'oldName': 'name'});
- *  // result: {oldName: 'Rex'}
+ *  transformByMap({oldName: 'Rex'}, {'newName': 'oldName'});
+ *  // result: {newName: 'Rex'}
+ * @example
+ *  transformByMap({ creds: { login: 'Rex' } }, { 'credentials.nickname': 'creds.login' });
+ *  // result: { credentials: { nickname: 'Rex' } }
  */
-export function transformByMap<T, R>(value: T, optionsMap: { [key in keyof R]: unknown }): R {
-    const result = {} as R;
-    for (const optionKey in optionsMap) {
-        if (Object.prototype.hasOwnProperty.call(optionsMap, optionKey)) {
-            const optionValue = optionsMap[optionKey];
+export function transformByMap<T, R, M>(data: T, mapper: M): R {
+    const result = {};
+    for (const optionKey in mapper) {
+        if (Object.prototype.hasOwnProperty.call(mapper, optionKey)) {
+            const optionValue = mapper[optionKey];
             if (typeof optionValue === 'string') {
-                result[optionKey] = get(value, optionValue);
+                const fieldValue: null | undefined = get(data, optionValue);
+                if (fieldValue) {
+                    set<R>(result, optionKey, fieldValue);
+                }
             }
         }
     }
 
-    return result;
+    return result as R;
 }
