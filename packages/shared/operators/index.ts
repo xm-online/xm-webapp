a155e45ebca0ee300a03b44378c9a9bbe25e4b89
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2020-10-29 10:24:36 +0200

    feat(shared-operation): add interpolate.ts

--- packages/shared/operators/index.ts
+++ packages/shared/operators/index.ts
@@ -3,3 +3,4 @@ export { debug } from './src/rxjs-debug';
 export { takeUntilOnDestroy, takeUntilOnDestroyDestroy } from './src/take-until-on-destroy';
 export { transformByMap } from './src/transform-by-map';
 export { uuid } from './src/uuid';
+export { Interpolate, interpolate } from './src/interpolate';
