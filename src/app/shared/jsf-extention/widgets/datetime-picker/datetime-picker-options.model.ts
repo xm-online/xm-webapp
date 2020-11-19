0091677aece92fc61d52bd65416f0a5be47c25a8
Andrew Kikot <andrew.kikot@gmail.com>
2020-11-19 12:41:22 +0200

    feat(datetime-picker): adding ability to format sending value

--- src/app/shared/jsf-extention/widgets/datetime-picker/datetime-picker-options.model.ts
+++ src/app/shared/jsf-extention/widgets/datetime-picker/datetime-picker-options.model.ts
@@ -2,6 +2,7 @@ export class DatetimePickerOptionsModel {
     constructor(public pickerType?: string,
                 public floatLabel?: string,
                 public themeColor?: string,
-                public formatString?: string) {
+                public formatString?: string,
+                public sendFormatString?: string) {
     }
 }
