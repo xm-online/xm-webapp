820ae9fc6e7bcdc77d9360d42cae6483e57f8766
Andrew Kikot <andrew.kikot@gmail.com>
2021-01-05 12:00:27 +0200

    feat(entity-list-fab): removing random entity generate option

--- src/app/xm-entity/entity-list-fab/entity-list-fab.component.ts
+++ src/app/xm-entity/entity-list-fab/entity-list-fab.component.ts
@@ -1,14 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { XmEventManager } from '@xm-ngx/core';
-import { XmToasterService } from '@xm-ngx/toaster';
 
 import { XM_EVENT_LIST } from '../../xm.constants';
 import { EntityDetailDialogComponent } from '../entity-detail-dialog/entity-detail-dialog.component';
 import { Spec } from '../shared/spec.model';
 import { XmEntitySpec } from '../shared/xm-entity-spec.model';
-import { XmEntitySpecService } from '../shared/xm-entity-spec.service';
-
 
 @Component({
     selector: 'xm-entity-list-fab',
@@ -20,22 +17,12 @@ export class EntityListFabComponent {
     @Input() public xmEntitySpec: XmEntitySpec;
     @Input() public spec: Spec;
 
-    constructor(private xmEntitySpecService: XmEntitySpecService,
-                private eventManager: XmEventManager,
-                private modalService: MatDialog,
-                private toasterService: XmToasterService) {
+    constructor(private eventManager: XmEventManager,
+                private modalService: MatDialog) {
     }
 
     public onRefresh(): void {
         this.eventManager.broadcast({name: XM_EVENT_LIST.XM_ENTITY_LIST_MODIFICATION});
-    }
-
-    public onGenerateNew(): void {
-        this.xmEntitySpecService.generateXmEntity(this.xmEntitySpec.key).toPromise().then((value) => {
-            this.eventManager.broadcast({name: XM_EVENT_LIST.XM_ENTITY_LIST_MODIFICATION});
-            this.toasterService.success('New entity "' + value.body.name + '" generated!');
-            this.toasterService.success('xm-entity.entity-list-fab.new-generated');
-        });
     }
 
     public onAddNew(): void {
