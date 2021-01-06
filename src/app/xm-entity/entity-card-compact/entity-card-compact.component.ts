fc0ffaf32dcaa6c8232ca50e63c80378259d971d
vkmita <victor.kmita@gmail.com>
2021-01-07 00:02:31 +0200

    feat(entity-card-compact): added recent updates with all dependencies

--- src/app/xm-entity/entity-card-compact/entity-card-compact.component.ts
+++ src/app/xm-entity/entity-card-compact/entity-card-compact.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { Component, EventEmitter, Input, OnInit, OnChanges, Output, ViewChild, SimpleChanges } from '@angular/core';
+//import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import {MatDialog, MatDialogRef} from '@angular/material/dialog';
 import { TranslateService } from '@ngx-translate/core';
 import { XmEventManager } from '@xm-ngx/core';
 import { XmToasterService } from '@xm-ngx/toaster';
@@ -10,22 +11,33 @@ import { EntityCardComponent } from '../entity-card/entity-card.component';
 import { RatingListSectionComponent } from '../rating-list-section/rating-list-section.component';
 import { XmEntityService } from '../shared/xm-entity.service';
 
+// transferred from entity-detail-fab
+import { EntityUiConfig } from '../../shared/spec/xm-ui-config-model';
+import { EntityDetailDialogComponent } from '../entity-detail-dialog/entity-detail-dialog.component';
+
+declare let swal: any;
 
 @Component({
     selector: 'xm-entity-card-compact',
     templateUrl: './entity-card-compact.component.html',
     styleUrls: ['./entity-card-compact.component.scss'],
 })
-export class EntityCardCompactComponent extends EntityCardComponent implements OnInit {
+export class EntityCardCompactComponent extends EntityCardComponent implements OnInit, OnChanges {
 
     @ViewChild('rating', {static: false}) public rating: RatingListSectionComponent;
 
     @Input() public preventDefaultUpdateError?: boolean;
+    @Input() public entityUiConfig: EntityUiConfig;
     @Output() public onSaveError: EventEmitter<boolean> = new EventEmitter<boolean>();
 
     public jsfAttributes: any;
     public showLoader: boolean;
     public isDescFull: boolean;
+
+    // transferred from entity-detail-fab
+    public showEditButton: boolean;
+    public showEditOptions: boolean = false;
+    public showEditSubOptions: boolean = false;
 
     constructor(
         protected modalService: MatDialog,
@@ -42,6 +54,56 @@ export class EntityCardCompactComponent extends EntityCardComponent implements O
         super.ngOnInit();
         this.loadJsfAttr();
     }
+
+// transferred from entity-detail-fab
+    public ngOnChanges(changes: SimpleChanges): void {
+        this.showEditButton = true;
+
+        const currentKey = changes.xmEntitySpec.currentValue.key;
+        const uiConfigKey = this.entityUiConfig && this.entityUiConfig.typeKey;
+
+        if (currentKey && uiConfigKey && currentKey === uiConfigKey) {
+            this.checkEntityEditPermission(this.entityUiConfig);
+        }
+    }
+
+// transferred from entity-detail-fab
+    private checkEntityEditPermission(config: EntityUiConfig): void {
+        if (config.editButtonPermission) {
+            this.showEditButton = false;
+            this.principal.hasPrivileges([config.editButtonPermission]).then((result) => {
+                if (result) {
+                    this.showEditButton = true;
+                }
+            });
+        }
+    }
+
+// transferred from entity-detail-fab
+    public onEdit(): void {
+        this.openDialog(EntityDetailDialogComponent, (modalRef) => {
+            modalRef.componentInstance.xmEntity = Object.assign({}, this.xmEntity);
+            modalRef.componentInstance.xmEntitySpec = this.xmEntitySpec;
+        });
+    }
+
+// transferred from entity-detail-fab
+    private openDialog(dialogClass: any, operation: any, options?: any): MatDialogRef<any> {
+        const modalRef = this.modalService.open<any>(dialogClass, options ? options : {width: '500px'});
+        modalRef.componentInstance.xmEntity = this.xmEntity;
+        operation(modalRef);
+        return modalRef;
+    }
+
+// transferred from entity-detail-fab
+    public xmEditContext(): () => any {
+        return () => {
+            // this flag turns off
+            this.showEditOptions = true;
+            return this.showEditOptions;
+        };
+    }
+
 
     public onSubmitForm(data: any): void {
         this.showLoader = true;
@@ -68,6 +130,15 @@ export class EntityCardCompactComponent extends EntityCardComponent implements O
             this.jsfAttributes = buildJsfAttributes(this.xmEntitySpec.dataSpec, this.xmEntitySpec.dataForm);
             this.jsfAttributes.data = Object.assign(nullSafe(this.jsfAttributes.data), nullSafe(this.xmEntity.data));
         }
+    }
+
+    protected alert(type: string, key: string): void {
+        swal({
+            type,
+            text: this.translateService.instant(key),
+            buttonsStyling: false,
+            confirmButtonClass: 'btn btn-primary',
+        });
     }
 
 }
