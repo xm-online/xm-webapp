fce3740589b3d2d55b570e8a42710293a2786f56
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2021-02-15 15:56:36 +0200

    test(dynamic): fix tests

--- packages/dynamic/src/loader/dynamic-loader.service.spec.ts
+++ packages/dynamic/src/loader/dynamic-loader.service.spec.ts
@@ -1,16 +1,24 @@
+import { NgModuleFactoryLoader } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+import { DynamicSearcher } from '@xm-ngx/dynamic';
 
 import { DynamicLoaderService } from './dynamic-loader.service';
 
 describe('DynamicLoaderService', () => {
-  let service: DynamicLoaderService;
+    let service: DynamicLoaderService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(DynamicLoaderService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                { provide: DynamicSearcher, useValue: null },
+                { provide: NgModuleFactoryLoader, useValue: null },
+                DynamicLoaderService,
+            ],
+        });
+        service = TestBed.inject(DynamicLoaderService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
--- packages/dynamic/src/loader/dynamic-tenant-loader.service.spec.ts
+++ packages/dynamic/src/loader/dynamic-tenant-loader.service.spec.ts
@@ -1,16 +1,24 @@
+import { NgModuleFactoryLoader } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+import { DynamicSearcher } from '@xm-ngx/dynamic';
 
 import { DynamicTenantLoaderService } from './dynamic-tenant-loader.service';
 
 describe('DynamicTenantLoaderService', () => {
-  let service: DynamicTenantLoaderService;
+    let service: DynamicTenantLoaderService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(DynamicTenantLoaderService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                { provide: DynamicSearcher, useValue: null },
+                { provide: NgModuleFactoryLoader, useValue: null },
+                DynamicTenantLoaderService,
+            ],
+        });
+        service = TestBed.inject(DynamicTenantLoaderService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
