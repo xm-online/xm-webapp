18048c55354af92b8f7efe08f2f5a432652dfd17
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2021-02-18 17:06:11 +0200

    chore(components-image): update naming

---
+++ packages/components/image/image/xm-image.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input } from '@angular/core';
+import { clone, defaults } from 'lodash';
+
+export interface XmImageOptions {
+    alt?: string;
+    imageClass?: string;
+    imageStyle?: string;
+    dataQa?: string;
+}
+
+export const XM_IMAGE_OPTIONS_DEFAULT: XmImageOptions = {
+    imageClass: 'img-fluid',
+    imageStyle: 'max-height: 40px;',
+    dataQa: 'image',
+    alt: ''
+};
+
+@Component({
+    selector: 'xm-image',
+    templateUrl: './xm-image.component.html',
+    styleUrls: ['./xm-image.component.scss'],
+})
+export class XmImageComponent {
+    @Input() public value: string;
+
+    private _options: XmImageOptions = clone(XM_IMAGE_OPTIONS_DEFAULT);
+
+    public get options(): XmImageOptions {
+        return this._options;
+    }
+
+    @Input()
+    public set options(value: XmImageOptions) {
+        this._options = defaults(value, XM_IMAGE_OPTIONS_DEFAULT);
+    }
+}
