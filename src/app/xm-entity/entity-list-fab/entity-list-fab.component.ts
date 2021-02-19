1773dfd38310981a96680b09a7051982f0edb74a
Dmitry Primak <dprimak.work@gmail.com>
2021-02-19 05:36:58 +0200

    feat(entity-list-fab): removing entity generate option for master

--- src/app/xm-entity/entity-list-fab/entity-list-fab.component.ts
+++ src/app/xm-entity/entity-list-fab/entity-list-fab.component.ts
@@ -1,17 +1,13 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { TranslateService } from '@ngx-translate/core';
 import { JhiEventManager } from 'ng-jhipster';
 
 import { XM_EVENT_LIST } from '../../xm.constants';
 import { EntityDetailDialogComponent } from '../entity-detail-dialog/entity-detail-dialog.component';
 import { Spec } from '../shared/spec.model';
 import { XmEntitySpec } from '../shared/xm-entity-spec.model';
-import { XmEntitySpecService } from '../shared/xm-entity-spec.service';
 import { Principal } from '../../shared';
 import { EntityUiConfig } from '../../shared/spec/xm-ui-config-model';
-
-declare let swal: any;
 
 @Component({
     selector: 'xm-entity-list-fab',
@@ -27,10 +23,8 @@ export class EntityListFabComponent implements OnChanges {
     @Input() public uiConfig: any;
     @Input() public entityType: any;
 
-    constructor(private xmEntitySpecService: XmEntitySpecService,
-                private eventManager: JhiEventManager,
+    constructor(private eventManager: JhiEventManager,
                 private modalService: NgbModal,
-                private translateService: TranslateService,
                 private principal: Principal,
     ) {
     }
@@ -65,23 +59,6 @@ export class EntityListFabComponent implements OnChanges {
 
     public onRefresh(): void {
         this.eventManager.broadcast({name: XM_EVENT_LIST.XM_ENTITY_LIST_MODIFICATION});
-    }
-
-    public onGenerateNew(): void {
-        this.xmEntitySpecService.generateXmEntity(this.xmEntitySpec.key).toPromise().then((value) => {
-            this.eventManager.broadcast({name: XM_EVENT_LIST.XM_ENTITY_LIST_MODIFICATION});
-            swal({
-                title: 'New entity "' + value.body.name + '" generated!',
-                buttonsStyling: false,
-                confirmButtonClass: 'btn btn-success',
-            });
-            swal({
-                type: 'success',
-                text: this.translateService.instant('xm-entity.entity-list-fab.new-generated'),
-                buttonsStyling: false,
-                confirmButtonClass: 'btn btn-primary',
-            });
-        });
     }
 
     public onAddNew(): void {
