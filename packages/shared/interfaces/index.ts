78957c7b9a9cf809ee0f5f48786b4f305cfc974f
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2021-02-26 11:38:41 +0200

    feat(shared-interfaces): add dataQa

--- packages/shared/interfaces/index.ts
+++ packages/shared/interfaces/index.ts
@@ -2,3 +2,4 @@ export { IId, Id } from './src/id';
 export { JavascriptCode } from './src/javascript-code';
 export { OnInitialize } from './src/on-initialize';
 export { Primitive } from './src/primitive';
+export { DataQa } from './src/data-qa';
---
+++ packages/shared/interfaces/src/data-qa.ts
@@ -0,0 +1,12 @@
+/**
+ * An interface for e2e tests
+ * @public
+ */
+export interface DataQa {
+    /**
+     * @example
+     * text-control
+     * submit-button
+     */
+    dataQa: string;
+}
