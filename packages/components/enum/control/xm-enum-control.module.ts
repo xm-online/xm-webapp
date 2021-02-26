d1044591f5adb925ed19243444d66fb1e5094ec1
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2021-02-26 13:01:48 +0200

    feat(enum): add example

---
+++ packages/components/enum/control/xm-enum-control.module.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { ControlErrorModule } from '@xm-ngx/components/control-error';
+import {
+    XmEnumControlComponent,
+    XmEnumControlOptions,
+} from './xm-enum-control.component';
+import { XmPermissionModule } from '@xm-ngx/core/permission';
+import { IControlFn } from '@xm-ngx/dynamic';
+import { XmTranslationModule } from '@xm-ngx/translation';
+
+@NgModule({
+    imports: [
+        XmTranslationModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        FormsModule,
+        MatIconModule,
+        CommonModule,
+        ControlErrorModule,
+        ReactiveFormsModule,
+        XmPermissionModule,
+    ],
+    exports: [XmEnumControlComponent],
+    declarations: [XmEnumControlComponent],
+})
+export class XmEnumControlModule {
+    public entry: IControlFn<string, XmEnumControlOptions> = XmEnumControlComponent;
+}
