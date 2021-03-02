1e3db1cf9d42f81175c5966f6096cadb958ab8a6
Andrew Kikot <andrew.kikot@gmail.com>
2021-03-02 08:39:48 +0200

    fix(entity-details): fix filtering spec backport (#895)

--- src/app/xm-entity/entity-detail-dialog/entity-detail-dialog.component.ts
+++ src/app/xm-entity/entity-detail-dialog/entity-detail-dialog.component.ts
@@ -70,8 +70,7 @@ export class EntityDetailDialogComponent implements OnInit, AfterViewInit {
         } else {
             if (this.spec && this.spec.types) {
                 this.availableSpecs = this.spec.types
-                    .filter((t) =>
-                        (!t.isAbstract && (t.key && (t.key.split('.')[0] === this.xmEntitySpec.key))));
+                    .filter((t) => this.filterSpecs(t));
                 this.xmEntity.key = UUID.UUID();
                 this.xmEntity.typeKey = this.availableSpecs[0].key;
                 this.onChangeEntityType(null, this.xmEntity.typeKey);
@@ -156,6 +155,12 @@ export class EntityDetailDialogComponent implements OnInit, AfterViewInit {
         if (this.smartDescription.active && this.smartDescription.template) {
             this.smartDescription.value = formatString(this.smartDescription.template, data);
         }
+    }
+
+    private filterSpecs(t: XmEntitySpec): boolean {
+        const matches = t.key.split(this.xmEntitySpec.key);
+        const matchNext = matches && (matches.length > 1) && matches[1];
+        return !t.isAbstract && ((matchNext && matchNext.startsWith('.')) || t.key === this.xmEntitySpec.key);
     }
 }
 
