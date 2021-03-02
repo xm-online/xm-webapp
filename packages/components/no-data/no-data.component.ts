ed518e79b2e27ce46ef866cf36f84b5046c6c1ae
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2021-03-02 12:31:48 +0200

    feat(no-data): add imageUrl

--- packages/components/no-data/no-data.component.ts
+++ packages/components/no-data/no-data.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { SafeUrl } from '@angular/platform-browser';
 import { Translate } from '@xm-ngx/translation';
 
 @Component({
@@ -7,6 +8,7 @@ import { Translate } from '@xm-ngx/translation';
     styleUrls: ['./no-data.component.scss'],
 })
 export class NoDataComponent {
+    @Input() public imageUrl: string | SafeUrl;
     @Input() public show: boolean;
     @Input() public text: Translate;
     @Input() public hideImage: boolean = false;
