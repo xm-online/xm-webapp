b8f8b80a2c87bc3836616c575c57d2e0b33a1657
Serhii Senko <serhii.senko@jevera.software>
2021-03-04 18:19:53 +0200

    update regexp in updateVersion

--- scripts/replace.build.js
+++ scripts/replace.build.js
@@ -1,5 +1,5 @@
-var replace = require('replace-in-file');
-var buildTimestamp = new Date().getTime();
+const replace = require('replace-in-file');
+const buildTimestamp = new Date().getTime();
 const options = {
     files: ['src/environments/environment.ts', 'src/environments/environment.prod.ts'],
     from: /buildTimestamp: '\d*'/g,
@@ -8,8 +8,8 @@ const options = {
 };
 
 try {
-    let changedFiles = replace.sync(options);
-    console.log('Build timestamp set: ' + buildTimestamp);
+    replace.sync(options);
+    console.info('Build timestamp set: ' + buildTimestamp);
 }
 catch (error) {
     console.error('Error occurred:', error);
