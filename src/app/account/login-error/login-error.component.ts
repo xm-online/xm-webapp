675129642bf3637406c9260f51774c1ebf39ee59
Andrew Kikot <andrew.kikot@gmail.com>
2021-03-05 16:10:10 +0200

    refactor(login-error): login oath2 error on navigate by direct url

--- src/app/account/login-error/login-error.component.ts
+++ src/app/account/login-error/login-error.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { combineLatest } from 'rxjs';
-import { XmUiConfigService } from '@xm-ngx/core/config';
 import { takeUntilOnDestroy, takeUntilOnDestroyDestroy } from '@xm-ngx/shared/operators';
 import { take } from 'rxjs/operators';
-import { IIdpClient } from '../../../../packages/core/src/xm-public-idp-config-model';
 import { LoginService } from '@xm-ngx/core/auth';
 
 @Component({
@@ -17,36 +14,17 @@ export class LoginErrorComponent implements OnDestroy {
 
     constructor(
         protected route: ActivatedRoute,
-        protected xmUiConfigService: XmUiConfigService,
         protected loginService: LoginService,
     ) {
-        combineLatest(
-            this.route.params,
-            this.xmUiConfigService.config$(),
-        )
+        this.route.params
             .pipe(
                 takeUntilOnDestroy(this),
                 take(1),
             )
-            .subscribe(([params, config]) => {
-                if (params?.idpKey && config?.idp?.enabled) {
-                    this.onLoginIdp(config?.idp?.clients?.filter((c: IIdpClient) => c.key === params?.idpKey)?.shift());
-                } else {
-                    this.errorKey = 'error.invalid_credentials';
-                }
-            });
+            .subscribe(() => this.errorKey = 'error.invalid_credentials');
     }
 
     public ngOnDestroy(): void {
         takeUntilOnDestroyDestroy(this);
-    }
-
-    private onLoginIdp(client: IIdpClient): void {
-        if (client) {
-            // this.loginService.loginWithIdpClient(client);
-            console.warn(client);
-        } else {
-            this.errorKey = 'login.messages.idp.wrong-key';
-        }
     }
 }
