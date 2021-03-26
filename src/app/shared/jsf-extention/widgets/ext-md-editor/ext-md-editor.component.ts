4de59d468957bc04dc14cfd81218f5600f47cfae
Arthur Briginets <artur.bryhynets@jevera.software>
2021-03-26 15:19:40 +0200

    fix(CVE-2018-19057): updated XSS fix

--- src/app/shared/jsf-extention/widgets/ext-md-editor/ext-md-editor.component.ts
+++ src/app/shared/jsf-extention/widgets/ext-md-editor/ext-md-editor.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { TdTextEditorComponent } from '@covalent/text-editor';
 import { JsonSchemaFormService } from 'angular2-json-schema-form';
 
+declare const $: any;
+
 @Component({
     selector: 'xm-ext-md-editor-widget',
     templateUrl: 'ext-md-editor.component.html',
@@ -21,8 +23,15 @@ export class ExtMdEditorComponent implements OnInit {
         showIcons: ['code', 'table'],
         previewRender: (plainText, preview) => {
             setTimeout(() => {
-                preview.innerHTML = this._textEditor.simpleMDE.markdown(plainText);
-                this._textEditor.writeValue(plainText.replace(/onerror=/ig, ''));
+                // for correct parsing in any cases we need to wrap plain text to root tag
+                if ($($.parseHTML(`<div>${plainText}</div>`)).find('img[onerror]').length > 0) {
+                    // this fix need for security issue: https://github.com/sparksuite/simplemde-markdown-editor/issues/721
+                    const errorText = 'onerror handler is not supported';
+                    preview.innerHTML = errorText;
+                    this._textEditor.writeValue(errorText);
+                } else {
+                    preview.innerHTML = this._textEditor.simpleMDE.markdown(plainText);
+                }
             });
         }
     };
