b88df0f9898d4f01e72fe83c28150560ae478e4b
Dmitry Primak <dprimak.work@gmail.com>
2021-03-29 14:36:42 +0300

    feat(content-uploader): changed to send on form submit

--- src/app/shared/jsf-extention/widgets/content-uploader/content-uploader-options.model.ts
+++ src/app/shared/jsf-extention/widgets/content-uploader/content-uploader-options.model.ts
@@ -5,6 +5,5 @@ export interface ContentUploaderOptions {
     noTitle?: boolean;
     placeholder?: string;
     maxFileSize?: number;
-    url?: string;
     validationMessages?: {required?: string; maxFileSize?: string};
 }
--- src/app/shared/jsf-extention/widgets/content-uploader/content-uploader.component.ts
+++ src/app/shared/jsf-extention/widgets/content-uploader/content-uploader.component.ts
@@ -1,5 +1,4 @@
-import { HttpClient, HttpEventType, HttpHeaders } from '@angular/common/http';
-import { ChangeDetectorRef, Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { JsonSchemaFormService } from '@xm-ngx/json-schema-form/core';
 import { ContentUploaderLayoutNode } from './content-uploader.layoutNode';
 import { ContentUploaderOptions } from './content-uploader-options.model';
@@ -21,9 +20,7 @@ export class ContentUploaderComponent implements OnInit, OnDestroy {
 
     @Input() public layoutNode: ContentUploaderLayoutNode;
 
-    constructor(private jsf: JsonSchemaFormService,
-                private changeDetectorRef: ChangeDetectorRef,
-                private httpClient: HttpClient) {
+    constructor(private jsf: JsonSchemaFormService) {
     }
 
     public ngOnInit(): void {
@@ -37,25 +34,15 @@ export class ContentUploaderComponent implements OnInit, OnDestroy {
         }
     }
 
-    public updateFile(event: any): void {
-        if (event.target.files.length > 0 && event.target.files[0].size <= this.options.maxFileSize) {
-            this.resetFile();
-            this.file = event.target.files[0];
-        } else if (event.target.files[0].size >= this.options.maxFileSize) {
+    public async updateFile(event: any): Promise<void> {
+        const files = event.target.files;
+        if (files.length > 0 && files[0].size <= this.options.maxFileSize) {
+            this.uploadingSizeError = false;
+            this.jsf.updateValue(this, await this.convertBase64(files[0]));
+            this.file = files[0];
+        } else if (files[0].size >= this.options.maxFileSize) {
             this.uploadingSizeError = true;
         }
-    }
-
-    public async uploadFile(fileData: any): Promise<void> {
-        if (!fileData) {return undefined; }
-        const file = fileData;
-        const fileBase64 = await this.convertBase64(file);
-        const formData: FormData = new FormData();
-        formData.append('file', fileBase64);
-        formData.append('fileName', file.name);
-
-        this.saveFile(formData);
-
     }
 
     public resetFile(): void {
@@ -74,51 +61,5 @@ export class ContentUploaderComponent implements OnInit, OnDestroy {
             reader.onload = () => resolve(reader.result.toString());
             reader.onerror = error => reject(error);
         });
-    }
-
-    private saveFile(formData: FormData, headers?: HttpHeaders): void {
-        const apiUrl = this.options.url || null;
-        this.uploadingError = false;
-        this.uploadingSizeError = false;
-        if (apiUrl) {
-            this.uploadProcess = this.httpClient
-                .post(apiUrl, formData,
-                    {
-                        headers,
-                        reportProgress: true,
-                        observe: 'events',
-                    },
-                ).subscribe((event) => {
-                    if (event.type === HttpEventType.UploadProgress) {
-                        this.updateProgress(event);
-                    } else if (event.type === HttpEventType.Response) {
-                        this.updateData(event.body);
-                    }
-                }, (err) => {
-                    this.handleError(err);
-                });
-        }
-    }
-
-    private updateProgress(event: any): void {
-        this.progress = Math.round(100 * event.loaded / event.total);
-        this.registerChanges();
-    }
-
-    private updateData(res: any): void {
-        this.jsf.updateValue(this, res.data.key);
-        this.progress = null;
-        this.registerChanges();
-    }
-
-    private handleError(err?: any): void {
-        console.warn(err);
-        this.uploadingError = true;
-        this.progress = null;
-        this.registerChanges();
-    }
-
-    private registerChanges(): void {
-        this.changeDetectorRef.detectChanges();
     }
 }
