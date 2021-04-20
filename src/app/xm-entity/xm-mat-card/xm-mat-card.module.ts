a05479acbab8bb153295d5d84243250414776cca
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2021-04-20 15:20:03 +0300

    feat(xm-mat-card): add loading

--- src/app/xm-entity/xm-mat-card/xm-mat-card.module.ts
+++ src/app/xm-entity/xm-mat-card/xm-mat-card.module.ts
@@ -5,6 +5,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { ConditionModule } from '@xm-ngx/components/condition';
 import { EditWidgetButtonsModule } from '@xm-ngx/components/edit-buttons';
+import { XmLoadingModule } from '@xm-ngx/components/loading';
 import { XmTranslationModule } from '@xm-ngx/translation';
 import { XmPermissionModule } from '../../../../packages/core/permisson';
 import { XmMatCardComponent } from './xm-mat-card.component';
@@ -22,6 +23,7 @@ import { XmMatCardComponent } from './xm-mat-card.component';
     MatButtonModule,
     XmTranslationModule,
     XmPermissionModule,
+    XmLoadingModule,
   ],
 })
 export class XmMatCardModule {
