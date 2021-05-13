dd6107dd4ce09dbe81159e1ec4b8084ce39fc6cc
Andrew Kikot <andrew.kikot@gmail.com>
2021-05-13 17:00:53 +0300

    feat(calendar): readonly feature for calendar

--- src/app/xm-entity/shared/calendar.model.ts
+++ src/app/xm-entity/shared/calendar.model.ts
@@ -5,6 +5,7 @@ import { XmEntity } from './xm-entity.model';
 export interface Calendar extends BaseEntity {
     id?: number;
     uuid?: string;
+    readonly?: boolean;
     typeKey?: string;
     name?: string;
     description?: string;
