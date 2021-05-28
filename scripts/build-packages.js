99f209dbe17fff8d05755c90c8b1505d3da81a12
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2021-05-28 19:58:49 +0300

    refactor(build): sync build

--- scripts/build-packages.js
+++ scripts/build-packages.js
@@ -1,28 +1,9 @@
 // TODO: migrate to bazel or alternative
-const {spawn} = require('child_process');
+const {execSync} = require('child_process');
 
-function execute(cmd, allowFailure = false) {
-    const ls = spawn(cmd, {shell: true});
-    ls.stdout.on('data', (data) => {
-        console.log(`stdout: ${data}`);
-    });
-
-    ls.stderr.on('data', (data) => {
-        console.error(`stderr: ${data}`);
-    });
-
-    ls.on('close', (code) => {
-        console.log(`child process exited with code ${code}`);
-        if (code !== 0 && allowFailure === false) {
-            process.exit(1);
-        }
-    });
-
-}
-
-execute('(cd ./packages/cli && npm run build)', false);
-execute('(cd ./packages/shared && npm run build)', false);
-execute('(ng-packagr -p ./packages/dynamic -c tsconfig.ngc.json)', false);
-execute('(ng-packagr -p ./packages/core -c tsconfig.ngc.json)', true);
-execute('(ng-packagr -p ./packages/components -c tsconfig.ngc.json)', true);
-execute('(ng-packagr -p ./packages/translation -c tsconfig.ngc.json)', true);
+execSync('(cd ./packages/cli && npm run build)', {stdio: 'inherit'});
+execSync('(cd ./packages/shared && npm run build)', {stdio: 'inherit'});
+execSync('(ng-packagr -p ./packages/dynamic -c tsconfig.ngc.json)', {stdio: 'inherit'});
+execSync('(ng-packagr -p ./packages/core -c tsconfig.ngc.json)', {stdio: 'inherit'});
+execSync('(ng-packagr -p ./packages/translation -c tsconfig.ngc.json)', {stdio: 'inherit'});
+// execSync('(ng-packagr -p ./packages/components -c tsconfig.ngc.json)', true)
