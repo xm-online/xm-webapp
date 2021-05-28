b4b9e890dfbf10679c2034f4609dea738b3858ef
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2021-05-28 19:14:11 +0300

    feat(build): strict lib execute

--- scripts/build-packages.js
+++ scripts/build-packages.js
@@ -1,8 +1,8 @@
 // TODO: migrate to bazel or alternative
-const { spawn } = require('child_process');
+const {spawn} = require('child_process');
 
-function execute(cmd) {
-    const ls = spawn(cmd, { shell: true });
+function execute(cmd, allowFailure = false) {
+    const ls = spawn(cmd, {shell: true});
     ls.stdout.on('data', (data) => {
         console.log(`stdout: ${data}`);
     });
@@ -13,13 +13,16 @@ function execute(cmd) {
 
     ls.on('close', (code) => {
         console.log(`child process exited with code ${code}`);
+        if (code !== 0 && allowFailure === false) {
+            process.exit(1);
+        }
     });
 
 }
 
-execute('(cd ./packages/cli && npm run build)');
-execute('(cd ./packages/shared && npm run build)');
-execute('(ng-packagr -p ./packages/dynamic -c tsconfig.ngc.json)');
-execute('(ng-packagr -p ./packages/core -c tsconfig.ngc.json)');
-execute('(ng-packagr -p ./packages/components -c tsconfig.ngc.json)');
-execute('(ng-packagr -p ./packages/translation -c tsconfig.ngc.json)');
+execute('(cd ./packages/cli && npm run build)', false);
+execute('(cd ./packages/shared && npm run build)', false);
+execute('(ng-packagr -p ./packages/dynamic -c tsconfig.ngc.json)', false);
+execute('(ng-packagr -p ./packages/core -c tsconfig.ngc.json)', true);
+execute('(ng-packagr -p ./packages/components -c tsconfig.ngc.json)', true);
+execute('(ng-packagr -p ./packages/translation -c tsconfig.ngc.json)', true);
