a29b72787901a156ffd622f4de500c087f53060d
EKrasovskiy <eugenekrasovskiy@gmail.com>
2021-06-07 15:47:40 +0300

    feat(error-handler): change reload page to logout

--- packages/core/global-error-handler/global-error-handler.ts
+++ packages/core/global-error-handler/global-error-handler.ts
@@ -1,13 +1,18 @@
 import { ErrorHandler, Injectable } from '@angular/core';
+import { XmSessionService } from '@xm-ngx/core';
 
 @Injectable()
 export class GlobalErrorHandler extends ErrorHandler {
+
+    constructor(private sessionService: XmSessionService) {
+        super();
+    }
 
     public handleError(error: any): void {
         const chunkFailedMessage = /Loading chunk [\d]+ failed/;
 
         if (chunkFailedMessage.test(error.message)) {
-            window.location.reload();
+            this.sessionService.clear();
         } else {
             super.handleError(error);
         }
