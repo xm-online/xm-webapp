f3121aabfc32603b6a6c9d833b241ebcef555303
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2021-07-07 11:29:13 +0300

    fix(environment): fix package import

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import * as npmPackage from '.././../package.json';
+import npmPackage from '.././../package.json';
 import { IEnvironment } from './models';
 
 export const environment: IEnvironment = {
