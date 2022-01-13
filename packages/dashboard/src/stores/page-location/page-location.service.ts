ba550358b3120a5e94bb397250e76a813910a280
zverbeta <shutko.yaroslav@gmail.com>
2022-01-13 14:26:19 +0200

    feat(page-location): support route params

--- packages/dashboard/src/stores/page-location/page-location.service.ts
+++ packages/dashboard/src/stores/page-location/page-location.service.ts
@@ -1,8 +1,8 @@
 import { Injectable, OnDestroy } from '@angular/core';
-import { ActivatedRoute, NavigationStart, Params, Router } from '@angular/router';
+import { ActivatedRoute, ActivatedRouteSnapshot, NavigationStart, Params, Router } from '@angular/router';
 import { IId } from '@xm-ngx/shared/interfaces';
 import { takeUntilOnDestroy, takeUntilOnDestroyDestroy } from '@xm-ngx/shared/operators';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
 import { filter, takeUntil } from 'rxjs/operators';
 
 interface PageEntityParams extends Params, IId {
@@ -30,9 +30,35 @@ export class PageLocationService implements OnDestroy {
         takeUntilOnDestroyDestroy(this);
     }
 
+    public getParams(): PageEntityParams | null {
+        let route = this.activatedRoute.snapshot.root;
+
+        const routes: ActivatedRouteSnapshot[] = [route];
+
+        while ((route = route.firstChild)) {
+            routes.push(route);
+        }
+
+        // TODO: (yaroslav) careful, nested params will be override
+        const params = Object.assign({}, ...routes.map(route => route.params));
+
+        return getId(params);
+    }
+
     public getQueryParams(): PageEntityParams | null {
         const queryParams = { ...this.activatedRoute.snapshot.queryParams };
+
         return getId(queryParams);
+    }
+
+    /**
+     * Prefer query params for page details, but also support basic segment params
+     */
+    public getPageParams(): PageEntityParams | null {
+        const queryParamsEntity = this.getQueryParams();
+        const paramsEntity = this.getParams();
+
+        return queryParamsEntity || paramsEntity;
     }
 
     /**
@@ -47,9 +73,10 @@ export class PageLocationService implements OnDestroy {
     }
 
     private init(): void {
-        this.change = new BehaviorSubject<PageEntityParams>(this.getQueryParams());
-        this.activatedRoute.queryParams
+        this.change = new BehaviorSubject<PageEntityParams>(this.getPageParams());
+
+        combineLatest([this.activatedRoute.queryParams, this.activatedRoute.params])
             .pipe(takeUntilOnDestroy(this))
-            .subscribe((res) => this.change.next(getId(res)));
+            .subscribe((_) => this.change.next(this.getPageParams()));
     }
 }
