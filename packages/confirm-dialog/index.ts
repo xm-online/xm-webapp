514c9dc0d740f0b86f4678373746a76dee81a0b8
zverbeta <shutko.yaroslav@gmail.com>
2022-05-27 09:38:37 +0300

    fix(confirm-dialog): fix confirm dialog imports

--- packages/confirm-dialog/index.ts
+++ packages/confirm-dialog/index.ts
@@ -1,3 +1,10 @@
-export * from './src/confirm-dialog.module';
-export * from './src/confirm-dialog.service';
-export * from './src/confirm-dialog.interface';
+export { XmConfirmDialogModule } from './src/confirm-dialog.module';
+export { XmConfirmDialogService } from './src/confirm-dialog.service';
+export {
+    XmConfirmDialogControls,
+    XmConfirmDialogControlDef,
+    XmConfirmDialogConditionModel,
+    XmConfirmDialogData,
+    XmConfirmDialogGroup,
+    XmConfirmDialogOptions,
+} from './src/confirm-dialog.interface';
