a6372d356804a6414ea44d87b0ef0665a34832bd
zverbeta <shutko.yaroslav@gmail.com>
2022-07-07 18:55:15 +0300

    feat(ajsf-handle-drag): add custom handle for drag elements

--- packages/json-schema-form/core/xm-json-schema-form.module.ts
+++ packages/json-schema-form/core/xm-json-schema-form.module.ts
@@ -5,13 +5,46 @@ import {
     JsonSchemaFormService,
     WidgetLibraryService,
 } from '@ajsf/core';
-import { MaterialDesignFramework, MaterialDesignFrameworkModule } from '@ajsf/material';
+import {
+    MaterialDesignFrameworkModule,
+} from '@ajsf/material';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RU_INTL } from '@xm-ngx/json-schema-form/locale/ru-config';
 import { UK_INTL } from '@xm-ngx/json-schema-form/locale/uk-config';
 import { LanguageService } from '@xm-ngx/translation';
+import { XmMaterialDesignFramework } from '@xm-ngx/json-schema-form/core/material-design-framework';
 import { fixFlexLayout } from './fix-flex-layout';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { XmFlexLayoutSectionComponent } from '@xm-ngx/json-schema-form/components/flex-layout-section.component';
+import { XmMaterialDesignFrameworkComponent } from '@xm-ngx/json-schema-form/components/material-design-framework.component';
+import { XmFlexLayoutRootComponent } from '@xm-ngx/json-schema-form/components/flex-layout-root.component';
+
+
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { XmJsonSchemaFormService } from '@xm-ngx/json-schema-form/core/xm-json-schema-form.service';
 
 const _JsonSchemaFormModule = {
     ngModule: JsonSchemaFormModule,
@@ -19,21 +52,47 @@ const _JsonSchemaFormModule = {
         JsonSchemaFormService,
         FrameworkLibraryService,
         WidgetLibraryService,
-        { provide: Framework, useClass: MaterialDesignFramework, multi: true },
+        { provide: Framework, useClass: XmMaterialDesignFramework, multi: true },
     ],
 };
 
 fixFlexLayout();
 
+export const ANGULAR_MATERIAL_MODULES = [
+    MatAutocompleteModule, MatButtonModule, MatButtonToggleModule, MatCardModule,
+    MatCheckboxModule, MatChipsModule, MatDatepickerModule, MatExpansionModule,
+    MatFormFieldModule, MatIconModule, MatInputModule, MatNativeDateModule,
+    MatRadioModule, MatSelectModule, MatSliderModule, MatSlideToggleModule,
+    MatStepperModule, MatTabsModule, MatTooltipModule,
+    MatToolbarModule, MatMenuModule, MatToolbarModule,
+];
+
+
 @NgModule({
+    declarations: [
+        XmFlexLayoutRootComponent,
+        XmFlexLayoutSectionComponent,
+        XmMaterialDesignFrameworkComponent,
+    ],
     imports: [
         CommonModule,
+        DragDropModule,
+        FlexLayoutModule,
+        ReactiveFormsModule,
+        FormsModule,
         MaterialDesignFrameworkModule,
+        ...ANGULAR_MATERIAL_MODULES,
         _JsonSchemaFormModule,
     ],
     exports: [
         MaterialDesignFrameworkModule,
         JsonSchemaFormModule,
+    ],
+    providers: [
+        {
+            provide: JsonSchemaFormService,
+            useExisting: XmJsonSchemaFormService,
+        },
     ],
 })
 export class XmJsonSchemaFormModule {
---
+++ packages/json-schema-form/core/xm-json-schema-form.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import {
+    getControl,
+    JsonPointer,
+    JsonSchemaFormService,
+} from '@ajsf/core';
+import { Subject } from 'rxjs';
+import { FormArray, FormControl } from '@angular/forms';
+
+export interface JsonSchemaOrderChange {
+    previousIndex: number;
+    currentIndex: number;
+}
+
+@Injectable({
+    providedIn: 'root',
+})
+export class XmJsonSchemaFormService extends JsonSchemaFormService {
+    public orderChanges = new Subject<JsonSchemaOrderChange>();
+
+    public moveArrayItemAndUpdateIndex({layoutIndex, dataIndex, layout}: {
+        layout: any[],
+        layoutIndex: number[],
+        dataIndex: number[]
+    }, previousIndex: number, currentIndex: number): boolean {
+        const { moveNodePointers }: {moveNodePointers: string[]} = this.formOptions;
+
+        const moved = this.moveArrayItem({
+            layoutNode: layout[previousIndex],
+            layoutIndex,
+            dataIndex,
+        }, previousIndex, currentIndex);
+
+        if (moved) {
+            const formArray = this.getFormControlGroup({
+                layoutNode: layout[previousIndex],
+                layoutIndex,
+                dataIndex,
+            });
+
+            if (!formArray || !(formArray instanceof FormArray)) {
+                return false;
+            }
+
+            formArray.controls.forEach((formGroup, index) => {
+                moveNodePointers
+                    ?.forEach((dataPointer) => {
+                        if (!JsonPointer.isJsonPointer(dataPointer)) {
+                            return;
+                        }
+
+                        const formControl = getControl(formGroup, JsonPointer.toKey(dataPointer));
+
+                        if (formControl && formControl instanceof FormControl) {
+                            formControl.patchValue(index);
+                        }
+                    });
+            });
+
+            this.orderChanges.next({
+                currentIndex,
+                previousIndex,
+            });
+
+            return true;
+        }
+
+        return false;
+    }
+}
