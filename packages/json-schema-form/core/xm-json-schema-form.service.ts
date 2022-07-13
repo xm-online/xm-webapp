b55f087ac98c6cf87d260bc5afdcadec5d673609
zverbeta <shutko.yaroslav@gmail.com>
2022-07-13 17:04:25 +0300

    fix(ajsf-build-layout): fix build layout

--- packages/json-schema-form/core/xm-json-schema-form.service.ts
+++ packages/json-schema-form/core/xm-json-schema-form.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import {
     getControl,
+    buildLayout,
     JsonPointer,
     JsonSchemaFormService,
 } from '@ajsf/core';
 import { Subject } from 'rxjs';
 import { FormArray, FormControl } from '@angular/forms';
+import { fixNestedBuildLayout } from '@xm-ngx/json-schema-form/shared/fix-build-layout';
 
 export interface JsonSchemaOrderChange {
     previousIndex: number;
@@ -23,7 +25,7 @@ export class XmJsonSchemaFormService extends JsonSchemaFormService {
         layoutIndex: number[],
         dataIndex: number[]
     }, previousIndex: number, currentIndex: number): boolean {
-        const { moveNodePointers }: {moveNodePointers: string[]} = this.formOptions;
+        const { moveNodePointers }: { moveNodePointers: string[] } = this.formOptions;
 
         const moved = this.moveArrayItem({
             layoutNode: layout[previousIndex],
@@ -66,5 +68,16 @@ export class XmJsonSchemaFormService extends JsonSchemaFormService {
         }
 
         return false;
+    }
+
+    public buildLayout(widgetLibrary: any): void {
+        this.layout = buildLayout(this, widgetLibrary);
+
+        if (this.formValues) {
+            fixNestedBuildLayout({
+                builtLayout: this.layout,
+                formData: this.formValues,
+            });
+        }
     }
 }
