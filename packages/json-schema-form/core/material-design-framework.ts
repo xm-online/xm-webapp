a5befbde56b04bd7547139b531b05e2b8186e585
zverbeta <shutko.yaroslav@gmail.com>
2022-07-21 18:27:18 +0300

    fix(disable-ajsf-override): temp disable ajsf override

--- packages/json-schema-form/core/material-design-framework.ts
+++ packages/json-schema-form/core/material-design-framework.ts
@@ -1,19 +1,19 @@
 import { Injectable } from '@angular/core';
 import { MaterialDesignFramework } from '@ajsf/material';
-import { XmFlexLayoutSectionComponent } from '@xm-ngx/json-schema-form/components/flex-layout-section.component';
-import { XmFlexLayoutRootComponent } from '@xm-ngx/json-schema-form/components/flex-layout-root.component';
-import { XmMaterialDesignFrameworkComponent } from '@xm-ngx/json-schema-form/components/material-design-framework.component';
+// import { XmFlexLayoutSectionComponent } from '@xm-ngx/json-schema-form/components/flex-layout-section.component';
+// import { XmFlexLayoutRootComponent } from '@xm-ngx/json-schema-form/components/flex-layout-root.component';
+// import { XmMaterialDesignFrameworkComponent } from '@xm-ngx/json-schema-form/components/material-design-framework.component';
 
 @Injectable()
 export class XmMaterialDesignFramework extends MaterialDesignFramework {
     constructor() {
         super();
 
-        this.framework = XmMaterialDesignFrameworkComponent;
-        this.widgets = {
-            ...this.widgets,
-            root: XmFlexLayoutRootComponent,
-            section: XmFlexLayoutSectionComponent,
-        };
+        // this.framework = XmMaterialDesignFrameworkComponent;
+        // this.widgets = {
+        //     ...this.widgets,
+        //     root: XmFlexLayoutRootComponent,
+        //     section: XmFlexLayoutSectionComponent,
+        // };
     }
 }
