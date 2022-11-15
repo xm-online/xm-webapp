95a0ff02991925315f52da108b7fe4977967d8b4
Андрей Гранковский <ahrankovskyi@itsmartflex.com>
2022-11-15 16:13:22 +0100

    fix(ссare): fix notification

--- proxy.conf.js
+++ proxy.conf.js
@@ -7,11 +7,22 @@ const PROXY_CONFIG = [
         pathRewrite: {
             [PROXY_SERVER_URL_REG]: '',
         },
-        target: 'https://xm.dev.xm-online.com/',
+        //target: "http://xm.test.xm-online.com.ua",
+        //target: 'http://pef-dev.itsf.dc',
+        //target: 'http://mw-dev-dua.itsf.dc',
+        //target: 'http://vm-mw-int-master1.dc:30000',
+        //target: 'https://neqsolsmart.com/',
+        //target: 'https://dspmwtst.vodafone.ua/',
+        //target: 'http://mw-dev-m2m.itsf.dc/',
+        //target: 'https://dspmw.vodafone.ua/',
+        target: 'https://dspmwtst.vodafone.ua/',
+        //target: 'https://dspmw.vodafone.ua/',
+        //target: 'http://mw-int.itsf.dc/',
+        //target: 'http://ang14-mw-dev-dua.itsf.dc/',
         secure: false,
         changeOrigin: true,
-        logLevel: 'debug',
-    },
+        logLevel: "debug"
+    }
 ];
 
 module.exports = PROXY_CONFIG;
