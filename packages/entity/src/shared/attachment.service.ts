22ce6f2b4a7982b27b1c3842d28a09dab19f5372
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2023-01-05 12:38:37 -0500

    refactor(general): remove SERVER_API_URL
    Signed-off-by: Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>

--- packages/entity/src/shared/attachment.service.ts
+++ packages/entity/src/shared/attachment.service.ts
@@ -5,14 +5,13 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { createRequestOption } from './request-util';
-import { SERVER_API_URL } from 'src/app/xm.constants';
 import { Attachment } from './attachment.model';
 
 @Injectable()
 export class AttachmentService {
 
-    private resourceUrl: string = SERVER_API_URL + 'entity/api/attachments';
-    private resourceSearchUrl: string = SERVER_API_URL + 'entity/api/_search/attachments';
+    private resourceUrl: string ='entity/api/attachments';
+    private resourceSearchUrl: string ='entity/api/_search/attachments';
 
     constructor(private http: HttpClient, private dateUtils: JhiDateUtils) {
     }
--- packages/entity/src/shared/comment.service.ts
+++ packages/entity/src/shared/comment.service.ts
@@ -5,14 +5,13 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { createRequestOption } from './request-util';
-import { SERVER_API_URL } from 'src/app/xm.constants';
 import { Comment } from './comment.model';
 
 @Injectable()
 export class CommentService {
 
-    private resourceUrl: string = SERVER_API_URL + 'entity/api/comments';
-    private resourceSearchUrl: string = SERVER_API_URL + 'entity/api/_search/comments';
+    private resourceUrl: string ='entity/api/comments';
+    private resourceSearchUrl: string ='entity/api/_search/comments';
 
     constructor(private http: HttpClient, private dateUtils: JhiDateUtils) {
     }
