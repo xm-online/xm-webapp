fe029889b7983d2112679af408ca47f3b3ee7ba8
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2023-01-16 10:33:51 -0500

    feature(docker): change dir to docker
    Signed-off-by: Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>

--- deploy/deploy-webapp-container.sh
+++ deploy/deploy-webapp-container.sh
@@ -4,22 +4,22 @@ if [ "$IMAGE_BRANCH" != "main" ]; then
 fi
 
 export DOCKER_REPO=$(echo -n $TRAVIS_REPO_SLUG | sed -e 's/^xm-online\//xmonline\//g')
+export DOCKER_DIR=deploy/webapp-container
 export IMAGE_BRANCH=$(echo -n $TRAVIS_BRANCH | sed -e 's/\//-/g')
 export PROJECT_VERSION=$(npm run-script get-version | tail -n1)
 export TAGS="$PROJECT_VERSION $PROJECT_VERSION-$TRAVIS_BUILD_NUMBER $(echo $PROJECT_VERSION | awk -F '.' '{printf $1"."$2" "$1}') latest"
 
-cp -r ./ docker/webapp-container
+cp -r ./ $DOCKER_DIR
 
 #docker login -u $DOCKER_USER -p $DOCKER_PASS
-#docker build -t app-docker-img \
-#  --label commit_id="$TRAVIS_COMMIT" \
-#  --label version="$PROJECT_VERSION" \
-#  --label build_number="$TRAVIS_BUILD_NUMBER" \
-#  --label build_url="$TRAVIS_BUILD_WEB_URL" \
-#  --label git_url="$TRAVIS_REPO_SLUG" \
-#  --label commit_message="$TRAVIS_COMMIT_MESSAGE" docker/webapp
+docker build -t app-docker-img \
+  --label commit_id="$TRAVIS_COMMIT" \
+  --label version="$PROJECT_VERSION" \
+  --label build_number="$TRAVIS_BUILD_NUMBER" \
+  --label build_url="$TRAVIS_BUILD_WEB_URL" \
+  --label git_url="$TRAVIS_REPO_SLUG" \
+  --label commit_message="$TRAVIS_COMMIT_MESSAGE" $DOCKER_DIR
 for TAG in $TAGS; do
-  echo TAG
-  #  docker tag app-docker-img $DOCKER_REPO:$TAG
-  #  docker push $DOCKER_REPO:$TAG
+  docker tag app-docker-img $DOCKER_REPO:$TAG
+  docker push $DOCKER_REPO:$TAG
 done
--- deploy/deploy-webapp.sh
+++ deploy/deploy-webapp.sh
@@ -7,14 +7,14 @@ if [ "$IMAGE_BRANCH" == "master" ] || [ "$IMAGE_BRANCH" == "main" ]; then
   PROJECT_VERSION=$(npm run-script get-version | tail -n1)
   TAGS="$PROJECT_VERSION $PROJECT_VERSION-$TRAVIS_BUILD_NUMBER $(echo $PROJECT_VERSION | awk -F '.' '{printf $1"."$2" "$1}') latest"
 fi
-cp -r dist/ docker/webapp
+cp -r dist/ deploy/webapp
 docker build -t app-docker-img \
   --label commit_id="$TRAVIS_COMMIT" \
   --label version="$PROJECT_VERSION" \
   --label build_number="$TRAVIS_BUILD_NUMBER" \
   --label build_url="$TRAVIS_BUILD_WEB_URL" \
   --label git_url="$TRAVIS_REPO_SLUG" \
-  --label commit_message="$TRAVIS_COMMIT_MESSAGE" docker/webapp
+  --label commit_message="$TRAVIS_COMMIT_MESSAGE" deploy/webapp
 for TAG in $TAGS; do
   docker tag app-docker-img $DOCKER_REPO:$TAG
   docker push $DOCKER_REPO:$TAG
