9eafef27f081f9abaa54b4d9d4ca4d0db33382d4
zverbeta <shutko.yaroslav@gmail.com>
2023-02-14 19:49:44 +0200

    feat(autocomplete-control): add autocomplete control

---
+++ packages/components/autocomplete-control/autocomplete-control.component.ts
@@ -0,0 +1,210 @@
+import { CommonModule } from '@angular/common';
+import {
+    Component,
+    forwardRef,
+    Input,
+    OnInit,
+    inject,
+} from '@angular/core';
+import { NG_VALUE_ACCESSOR, ReactiveFormsModule, FormControl } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { NgModelWrapper } from '@xm-ngx/components/ng-accessor';
+import { LanguageService, Translate, XmTranslationModule } from '@xm-ngx/translation';
+import * as _ from 'lodash';
+import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
+import { of } from 'rxjs';
+import { catchError, debounceTime, distinctUntilChanged, map, startWith, switchMap, tap, filter, shareReplay } from 'rxjs/operators';
+import { HintModule, HintText } from '@xm-ngx/components/hint';
+import { EntityCollectionFactoryService } from '@xm-ngx/components/entity-collection';
+import { format } from '@xm-ngx/shared/operators';
+import { HttpHeaders, HttpParams } from '@angular/common/http';
+
+export interface XmAutocompleteControlMapper {
+    // Interpolated string as ${name}
+    displayFn: string;
+    valueByKey: string;
+}
+
+export type XmAutocompleteControlParams = Record<string, string>;
+export type XmAutocompleteControlBody = Record<string, string>;
+
+export interface XmAutocompleteControlListItem {
+    value: string;
+    view: string;
+}
+
+export interface XmAutocompleteControlConfig {
+    hint?: HintText;
+    title?: string;
+    search: {
+        resourceUrl: string;
+        resourceMethod: string;
+        queryParams: XmAutocompleteControlParams;
+        body: XmAutocompleteControlBody;
+        headers: Record<string, string>;
+    };
+    extractByKey: string;
+    itemMapper: XmAutocompleteControlMapper;
+    autoSelectFirst: boolean;
+    searchIfEmpty: boolean;
+    searchPlaceholder: Translate;
+    notFoundSearchPlaceholder: Translate;
+}
+
+const AUTOCOMPLETE_CONTROL_DEFAULT_CONFIG: XmAutocompleteControlConfig = {
+    hint: null,
+    title: '',
+    search: {
+        resourceUrl: null,
+        resourceMethod: 'GET',
+        queryParams: {},
+        body: {},
+        headers: {},
+    },
+    autoSelectFirst: false,
+    searchIfEmpty: false,
+    extractByKey: null,
+    itemMapper: {
+        displayFn: '${name}',
+        valueByKey: '${name}',
+    },
+    searchPlaceholder: 'global.rest-select-placeholder-noresults',
+    notFoundSearchPlaceholder: 'global.rest-select-placeholder-search.simple',
+};
+
+@Component({
+    standalone: true,
+    selector: 'xm-autocomplete-control',
+    template: `
+        <mat-form-field>
+            <mat-label *ngIf="config?.title">{{ config?.title | translate }}</mat-label>
+
+            <mat-select [disabled]="disabled"
+                        [value]="value"
+                        (selectionChange)="change($event.value)">
+                <mat-option>
+                    <ngx-mat-select-search [formControl]="searchQueryControl"
+                                           [placeholderLabel]="config.searchPlaceholder | translate"
+                                           [noEntriesFoundLabel]="config.notFoundSearchPlaceholder | translate"></ngx-mat-select-search>
+                </mat-option>
+
+                <mat-option [hidden]="!value" (click)="deselect()">
+                    <mat-icon>close</mat-icon>
+                    {{'common-webapp-ext.buttons.cancel' | translate}}
+                </mat-option>
+
+                <mat-option *ngFor="let s of list | async"
+                            [value]="s.value">
+                    {{s.view | translate}}
+                </mat-option>
+            </mat-select>
+
+            <mat-hint [hint]="config.hint"></mat-hint>
+        </mat-form-field>
+    `,
+    imports: [
+        MatFormFieldModule,
+        MatSelectModule,
+        NgxMatSelectSearchModule,
+        ReactiveFormsModule,
+        XmTranslationModule,
+        MatIconModule,
+        CommonModule,
+        HintModule,
+    ],
+    providers: [ { provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => XmAutocompleteControlComponent), multi: true } ],
+})
+export class XmAutocompleteControlComponent extends NgModelWrapper<string> implements OnInit {
+    private _config: XmAutocompleteControlConfig = AUTOCOMPLETE_CONTROL_DEFAULT_CONFIG;
+
+    @Input()
+    public set config(value: XmAutocompleteControlConfig) {
+        this._config = _.defaultsDeep(value, AUTOCOMPLETE_CONTROL_DEFAULT_CONFIG) as XmAutocompleteControlConfig;
+    }
+    public get config(): XmAutocompleteControlConfig {
+        return this._config;
+    }
+
+    private collectionFactory = inject(EntityCollectionFactoryService);
+    private languageService = inject(LanguageService);
+
+    public searchQueryControl = new FormControl('');
+    public list = of<XmAutocompleteControlListItem[]>([]);
+
+    public ngOnInit(): void {
+        this.list = this.searchQueryControl.valueChanges.pipe(
+            startWith<string>(null),
+            distinctUntilChanged(),
+            debounceTime(300),
+            filter((value) => !_.isEmpty(value) || this.config?.searchIfEmpty),
+            switchMap((searchQuery) => {
+                const locale = this.languageService.locale;
+                const languages = this.languageService.languages;
+
+                const { resourceUrl, resourceMethod, queryParams, headers, body} = this.config?.search || {};
+
+                const httpParams: XmAutocompleteControlParams = format(queryParams, { search: searchQuery, locale, languages });
+                const httpBody: XmAutocompleteControlBody = format(body, { search: searchQuery, locale, languages });
+
+                if (resourceUrl) {
+                    return this.collectionFactory.create(resourceUrl).request(
+                        resourceMethod,
+                        httpBody,
+                        new HttpParams({
+                            fromObject: httpParams,
+                        }),
+                        new HttpHeaders(headers),
+                    ).pipe(
+                        catchError(() => of([])),
+                        map((data) => {
+                            if (this.config?.extractByKey) {
+                                return _.get(data, this.config?.extractByKey, []);
+                            }
+                            
+                            return data;
+                        }),
+                        map(collection => {
+                            if (!_.isEmpty(collection) && _.isArray(collection)) {
+                                return collection?.map((item) => {
+                                    const { displayFn, valueByKey } = format<XmAutocompleteControlMapper>(this.config?.itemMapper, {
+                                        locale,
+                                        languages,
+                                    });
+
+                                    return {
+                                        value: _.template(valueByKey)(item as object),
+                                        view: _.template(displayFn)(item as object),
+                                    } as XmAutocompleteControlListItem;
+                                });
+                            }
+                            
+                            return [];
+                        })
+                    );
+                }
+
+                return of([]);
+            }),
+            tap((list) => {
+                if (this.config?.autoSelectFirst) {
+                    this.autoSelectSingle(list);
+                }
+            }),
+            shareReplay(1),
+        );
+    }
+
+    private autoSelectSingle(list: XmAutocompleteControlListItem[]) {
+        if (list.length == 1) {
+            const [first] = list;
+
+            this.change(first.value);
+        }
+    }
+
+    public deselect(): void {
+        this.change('');
+    }
+}
--- src/registries/xm-array.registry.ts
+++ src/registries/xm-array.registry.ts
@@ -5,4 +5,8 @@ export const XM_ARRAY_ELEMENTS: XmDynamicEntry[] = [
         selector: '@xm-ngx/components/array-control',
         loadChildren: () => import('@xm-ngx/components/array-control').then(m => m.XmArrayControlModule),
     },
+    {
+        selector: '@xm-ngx/components/autocomplete-control',
+        loadChildren: () => import('@xm-ngx/components/autocomplete-control/autocomplete-control.component').then(m => m.XmAutocompleteControlComponent),
+    },
 ];
