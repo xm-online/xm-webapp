d08e3730e2cb2c1db736c248073c3d54590fdd95
GitHub Action <action@github.com>
2023-02-22 17:47:20 +0000

    Autoincrement version to 3.12.15
    Signed-off-by: Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>

--- packages/components/phone-number-control/xm-phone-number-control.component.spec.ts
+++ packages/components/phone-number-control/xm-phone-number-control.component.spec.ts
@@ -1,15 +1,12 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { ControlErrorModule } from '@xm-ngx/components/control-error';
-import { XmTranslationTestingModule } from '@xm-ngx/translation/testing';
-import { NgxMaskModule } from 'ngx-mask';
 
 import { XmPhoneNumberControlComponent } from './xm-phone-number-control.component';
+import { XmTranslationTestingModule } from '@xm-ngx/translation/testing';
+import { NgxMaskModule } from 'ngx-mask';
+import { ControlErrorModule } from '@xm-ngx/components/control-error';
 import { XM_VALIDATOR_PROCESSING_CONTROL_ERRORS_TRANSLATES } from '@xm-ngx/components/validator-processing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('XmPhoneNumberControlComponent', () => {
     let component: XmPhoneNumberControlComponent;
@@ -18,17 +15,12 @@ describe('XmPhoneNumberControlComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             imports: [
-                XmTranslationTestingModule,
-                MatFormFieldModule,
-                MatInputModule,
-                FormsModule,
-                ReactiveFormsModule,
-                ControlErrorModule,
-                ControlErrorModule.forRoot({errorTranslates: XM_VALIDATOR_PROCESSING_CONTROL_ERRORS_TRANSLATES}),
+                ControlErrorModule.forRoot({ errorTranslates: XM_VALIDATOR_PROCESSING_CONTROL_ERRORS_TRANSLATES }),
                 NgxMaskModule.forRoot(),
                 NoopAnimationsModule,
+                XmPhoneNumberControlComponent,
+                XmTranslationTestingModule,
             ],
-            declarations: [XmPhoneNumberControlComponent],
             schemas: [NO_ERRORS_SCHEMA],
         })
             .compileComponents();
