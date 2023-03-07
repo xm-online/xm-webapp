8bf562fb6dc812c4316ef029807359ef6db996c8
zverbeta <shutko.yaroslav@gmail.com>
2023-03-07 15:41:57 +0200

    feat(autocomplete-list-table): add autocomplete list table (#1589)
    * feat(autocomplete-list-table): add autocomplete list table
    
    * Autoincrement version to 4.0.4
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>

--- packages/components/xm-array.registry.ts
+++ packages/components/xm-array.registry.ts
@@ -9,4 +9,8 @@ export const XM_ARRAY_ELEMENTS: XmDynamicEntry[] = [
         selector: '@xm-ngx/components/autocomplete-control',
         loadChildren: () => import('@xm-ngx/components/autocomplete-control/autocomplete-control.component').then(m => m.XmAutocompleteControlComponent),
     },
+    {
+        selector: '@xm-ngx/components/autocomplete-table-control',
+        loadChildren: () => import('@xm-ngx/components/autocomplete-control/autocomplete-table-control.component').then(m => m.XmAutocompleteTableControlComponent),
+    },
 ];
