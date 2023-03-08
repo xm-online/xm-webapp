0eb1b5a2133d5c57cd471016746aff4efb51aa44
zverbeta <shutko.yaroslav@gmail.com>
2023-03-08 14:59:17 +0200

    fix(revert-changes): revert changes (#1591)
    * fix(revert-changes): revert changes
    
    * Autoincrement version to 4.0.5
    
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>

--- packages/dynamic/shared/set-component-input.ts
+++ packages/dynamic/shared/set-component-input.ts
@@ -3,15 +3,12 @@ import { ComponentRef } from '@angular/core';
 export function setComponentInput<T>(compRef: ComponentRef<T>, name: string, value: unknown): void {
     // Used for compatibility
     const inputs = (compRef?.['_tNode']?.['inputs'] ?? {}) as Record<string, string>;
-    const isInputDecoratorDeclared = inputs && inputs[name];
-    const isPropertyExist = compRef.instance[name] != undefined;
-    if (isInputDecoratorDeclared) {
+        
+    if (inputs && inputs[name]) {
         compRef.setInput(name, value);
     } else {
-        // TODO: Define hierarchy of components and required, optional fields validation.
-        if (isPropertyExist) {
-            console.warn(`No @Input() decorator present for component=${compRef.componentType.name}, field=${name}, used deprecated method.`);
-        }
+        console.warn(`No @Input() decorator present for component=${compRef.componentType.name}, field=${name}, used deprecated method.`);
+        
         try {
             compRef.instance[name] = value;
         } catch (error) {
