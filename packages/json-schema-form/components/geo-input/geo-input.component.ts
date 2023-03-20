425f3e712df79f873c173aa774f952ed5b7148c1
Andrew Kikot <andrew.kikot@jevera.software>
2023-03-20 14:36:01 +0200

    feat: adding libraries and language to map api resolver, input options and logic

--- packages/json-schema-form/components/geo-input/geo-input.component.ts
+++ packages/json-schema-form/components/geo-input/geo-input.component.ts
@@ -1,42 +1,62 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { GeoInputOptions } from './geo-input.model';
 import { JsonSchemaFormService } from '@xm-ngx/json-schema-form/core';
-import { LanguageService, Locale } from '@xm-ngx/translation';
 
 declare let google: any;
-export type LocaleCode = string | 'en-us' | 'ru-ru' | 'uk-ua' | 'de-de' | 'it';
+
+interface IPlaceValue {
+    formatted_address: string;
+    address_components: {
+        long_name: string;
+        short_name: string;
+        types: string[];
+    }[];
+    place_id: string;
+    geometry: IPlaceValueGeometry
+    types: string[];
+    utc_offset_minutes: number
+}
+
+interface IPlaceValueGeometry {
+    location: {
+        lat: number;
+        lng: number;
+    }
+}
 
 @Component({
     selector: 'geo-input',
     templateUrl: './geo-input.component.html',
 })
 export class GeoInputComponent implements OnInit {
-    public controlValue: string = '';
+    public controlValue!: string;
+    public modelValue: string = '';
     public options: GeoInputOptions;
-    public lang!: LocaleCode;
+
     @Input() public layoutNode: any;
     @ViewChild('geoInput') public geoInput: any;
 
     constructor(
         private jsf: JsonSchemaFormService,
-        private languageService: LanguageService,
     ) {
-        this.languageService.locale$.subscribe((l) => {
-            this.lang = this.getLangCode(l);
-        });
     }
 
     public ngOnInit(): void {
-        this.options = this.layoutNode.options || {};
-        this.jsf.initializeControl(this);
+        this.options = this.layoutNode?.options || {};
     }
 
     public onAfterGMapApiInit(): void {
         this.getPlaceAutocomplete();
+
+        this.jsf.initializeControl(this);
+        if (this.controlValue) {
+            const dataValue: IPlaceValue = JSON.parse(this.controlValue);
+            this.modelValue = dataValue?.formatted_address;
+        }
     }
 
     private getPlaceAutocomplete() {
-        const autocomplete = new google.maps.places.Autocomplete(this.geoInput.nativeElement, {types: this.options.predictionType || ['address']});
+        const autocomplete = new google.maps.places.Autocomplete(this.geoInput?.nativeElement, {types: this.options.predictionType || ['address']});
         google.maps.event.addListener(autocomplete, 'place_changed', () => {
             const place = autocomplete.getPlace();
             this.invokeEvent(place);
@@ -44,20 +64,31 @@ export class GeoInputComponent implements OnInit {
     }
 
     public invokeEvent(place: any): void {
-        this.jsf.updateValue(this, place?.formatted_address);
+        this.modelValue = place?.formatted_address;
+        const updatedValue = this.cookValue(place);
+        this.jsf.updateValue(this, updatedValue);
     }
 
-    private getLangCode(l: Locale = 'en'): LocaleCode {
-        switch (l) {
-            case 'en': {
-                return `${l}-us`;
-            }
-            case 'uk': {
-                return `${l}-ua`;
-            }
-            default: {
-                return `${l}-${l}`;
-            }
-        }
+    private cookValue(place: any): string {
+        const cooked: IPlaceValue = {
+            formatted_address: place?.formatted_address,
+            address_components: place?.address_components,
+            geometry: this.getGeometryData(place),
+            place_id: place?.place_id,
+            types: place?.types,
+            utc_offset_minutes: place?.utc_offset_minutes,
+        };
+        return JSON.stringify(cooked);
+    }
+
+    private getGeometryData(place): IPlaceValueGeometry {
+        const lat = place?.geometry?.location?.lat();
+        const lng = place?.geometry?.location?.lng();
+        return {
+            location: {
+                lat,
+                lng,
+            },
+        };
     }
 }
