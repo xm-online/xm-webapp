533a5f7ac83b85ebc7e4f3a8a4db478a3fb0b35c
zverbeta <shutko.yaroslav@gmail.com>
2023-04-05 13:34:28 +0300

    pass lint

--- packages/json-schema-form/components/xm-material-add-reference.component.ts
+++ packages/json-schema-form/components/xm-material-add-reference.component.ts
@@ -4,10 +4,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { NgClass, NgIf } from '@angular/common';
 
 @Component({
-  // tslint:disable-next-line:component-selector
-  selector: 'material-add-reference-widget',
-  standalone: true,
-  template: `
+    // tslint:disable-next-line:component-selector
+    selector: 'material-add-reference-widget',
+    standalone: true,
+    template: `
     <section [ngClass]="options?.htmlClass ?? ''" align="end">
       <button mat-raised-button *ngIf="showAddButton"
         [color]="options?.color || 'accent'"
@@ -17,8 +17,8 @@ import { NgClass, NgIf } from '@angular/common';
         <span *ngIf="options?.title" [innerHTML]="buttonText"></span>
       </button>
     </section>`,
-  imports: [NgIf, NgClass, MatButtonModule],
-  changeDetection: ChangeDetectionStrategy.Default,
+    imports: [NgIf, NgClass, MatButtonModule],
+    changeDetection: ChangeDetectionStrategy.Default,
 })
 export class XmMaterialAddReferenceComponent extends MaterialAddReferenceComponent {
 }
