b089e94ee16d438d548ff0ff17526bb56bb82f38
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2023-05-16 16:34:04 -0400

    refactor(exceptions): move to package
    Signed-off-by: Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>

---
+++ packages/exceptions/index.ts
@@ -0,0 +1,4 @@
+export { ArgumentException } from './src/argument-exception';
+export { NotImplementedException } from './src/not-implemented-exception';
+export { NotSupportedException } from './src/not-supported-exception';
+export { NotFoundException } from './src/not-found-exception';
