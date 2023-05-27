d3a2bfe8de11d0952a16b53e4c9ba5566dba2d5f
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2023-05-27 13:11:20 -0400

    fix(general): rollback to packages direct import
    Signed-off-by: Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>

--- scripts/build-packages.js
+++ scripts/build-packages.js
@@ -2,13 +2,13 @@
 const {execSync} = require('child_process');
 
 execSync('(cd ./packages/cli && npm run build && cd ../../)', {stdio: 'inherit'});
+execSync('(cd ./packages/eslint-plugin && npm run build && cd ../../)', {stdio: 'inherit'});
 execSync('(cd ./packages/styles && npm run build && cd ../../)', {stdio: 'inherit'});
 execSync('(cd ./packages/interfaces && npm run build && cd ../../)', {stdio: 'inherit'});
 execSync('(cd ./packages/exceptions && npm run build && cd ../../)', {stdio: 'inherit'});
 execSync('(cd ./packages/pipes && npm run build && cd ../../)', {stdio: 'inherit'});
 execSync('(cd ./packages/validators && npm run build && cd ../../)', {stdio: 'inherit'});
 execSync('(cd ./packages/operators && npm run build && cd ../../)', {stdio: 'inherit'});
-execSync('(cd ./packages/eslint-plugin && npm run build && cd ../../)', {stdio: 'inherit'});
 execSync('(cd ./packages/repositories && npm run build && cd ../../)', {stdio: 'inherit'});
 execSync('(cd ./packages/core && npm run build && cd ../../)', {stdio: 'inherit'});
 execSync('(cd ./packages/dynamic && npm run build && cd ../../)', {stdio: 'inherit'});
@@ -16,6 +16,7 @@ execSync('(cd ./packages/logger && npm run build && cd ../../)', {stdio: 'inheri
 execSync('(cd ./packages/translation && npm run build && cd ../../)', {stdio: 'inherit'});
 execSync('(cd ./packages/toaster && npm run build && cd ../../)', {stdio: 'inherit'});
 execSync('(cd ./packages/alert && npm run build && cd ../../)', {stdio: 'inherit'});
+
 // execSync('(ng-packagr -p ./packages/components -c tsconfig.ngc.json)', {stdio: 'inherit'});
 // execSync('(ng-packagr -p ./packages/confirm-dialog -c tsconfig.ngc.json)', {stdio: 'inherit'});
 // execSync('(cd ./packages/entity && npm run build && cd ../../)', {stdio: 'inherit'});
@@ -29,7 +30,7 @@ execSync('(cd ./packages/alert && npm run build && cd ../../)', {stdio: 'inherit
 // execSync('(ng-packagr -p ./packages/dashboard -c tsconfig.ngc.json)', {stdio: 'inherit'});
 // execSync('(ng-packagr -p ./packages/documentation -c tsconfig.ngc.json)', {stdio: 'inherit'});
 
-execSync('(cd ./src/app/ext/example-webapp-ext && npm run build && cd ../../../../)', {stdio: 'inherit'});
+// execSync('(cd ./src/app/ext/example-webapp-ext && npm run build && cd ../../../../)', {stdio: 'inherit'});
 
 // TODO:WORKAROUND fixing bin folder adding.
 execSync('(npm i --ignore-scripts)', {stdio: 'inherit'});
