cd6504a9bb5cbdcf03b6b4251a791de6db8df980
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2023-06-06 11:16:48 -0400

    feat(table): add multistore
    Signed-off-by: Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>

--- packages/components/table/controllers/config/xm-table-columns-setting-storage.service.ts
+++ packages/components/table/controllers/config/xm-table-columns-setting-storage.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { Translate } from '@xm-ngx/translation';
 import { LocalStorageService } from 'ngx-webstorage';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 export interface ColumnsSettingStorageItem {
     name: string;
@@ -10,30 +11,87 @@ export interface ColumnsSettingStorageItem {
     isHideLock: boolean;
 }
 
-export const COLUMNS_SETTING_STORE_NAME = '[columns setting] store';
+export const COLUMNS_SETTING_STORE_NAME = 'XmTableSettingStoreState';
+
+export interface XmTableSettingStoreStateItem {
+    columns: ColumnsSettingStorageItem[]
+}
+
+type XmTableSettingStoreState = Map<string, XmTableSettingStoreStateItem>;
 
 @Injectable({
     providedIn: 'root',
 })
-export class XmTableColumnsSettingStorageService {
-    private store: BehaviorSubject<ColumnsSettingStorageItem[]> =
-        new BehaviorSubject<ColumnsSettingStorageItem[]>(this.localStorage.retrieve(COLUMNS_SETTING_STORE_NAME) || []);
+export class XmTableSettingStore implements OnDestroy {
+    private readonly store: BehaviorSubject<XmTableSettingStoreState>;
 
     constructor(
         private localStorage: LocalStorageService,
     ) {
+        const state: XmTableSettingStoreState = this.localStorage.retrieve(COLUMNS_SETTING_STORE_NAME) || new Map();
+        this.store = new BehaviorSubject<XmTableSettingStoreState>(state);
+        this.store.subscribe(value => {
+            this.localStorage.store(COLUMNS_SETTING_STORE_NAME, value);
+        });
     }
 
-    public updateStore(items: ColumnsSettingStorageItem[]): void {
-        this.store.next(items);
-        this.localStorage.store(COLUMNS_SETTING_STORE_NAME, this.store.value);
+    public ngOnDestroy(): void {
+        this.store.complete();
+    }
+
+    public updateStore(key: string, value: XmTableSettingStoreStateItem): void {
+        const state = this.store.value;
+        state.set(key, value);
+        this.store.next(state);
+    }
+
+    public defaultStore(key: string, items: XmTableSettingStoreStateItem): void {
+        if (!this.store.value.has(key))
+            this.updateStore(key, items);
+    }
+
+
+    public getStore(key: string): Observable<XmTableSettingStoreStateItem> {
+        return this.store.pipe(
+            map(i => i.get(key)),
+        );
+    }
+
+    public clearStore(key: string): void {
+        const state = this.store.value;
+        state.delete(key);
+    }
+}
+
+@Injectable()
+export class XmTableColumnsSettingStorageService {
+
+
+    constructor(
+        private XmTableColumnsSettingStorageService: XmTableSettingStore,
+    ) {
+    }
+
+    private get key(): string {
+        // TODO: improve make a unique location for each table
+        return location.pathname;
+    }
+
+    public updateStore(columns: ColumnsSettingStorageItem[]): void {
+        this.XmTableColumnsSettingStorageService.updateStore(this.key, { columns });
+    }
+
+    public defaultStore(columns: ColumnsSettingStorageItem[]): void {
+        this.XmTableColumnsSettingStorageService.defaultStore(this.key, { columns });
     }
 
     public getStore(): Observable<ColumnsSettingStorageItem[]> {
-        return this.store.asObservable();
+        return this.XmTableColumnsSettingStorageService.getStore(this.key).pipe(
+            map(i => i.columns),
+        );
     }
 
     public clearStore(): void {
-        this.localStorage.clear(COLUMNS_SETTING_STORE_NAME);
+        this.XmTableColumnsSettingStorageService.clearStore(this.key);
     }
 }
