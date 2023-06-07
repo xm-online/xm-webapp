39e2a2881da1fc42cce32c0eaee90e955381a3df
zverbeta <shutko.yaroslav@gmail.com>
2023-06-07 13:20:52 +0300

    feat(autocomplete-control): format backend data from config

--- packages/components/autocomplete-control/autocomple-control.interface.ts
+++ packages/components/autocomplete-control/autocomple-control.interface.ts
@@ -38,6 +38,7 @@ export interface XmAutocompleteControlConfig {
     extractByKey?: string;
     compareMap: XmFormatTemplateRecursive;
     itemMapper: XmAutocompleteControlMapper;
+    formatBackendData: object;
     pickIntersectSelected?: boolean;
     skipFetchSelected?: boolean;
     valueAsJson?: boolean;
@@ -75,6 +76,7 @@ export const AUTOCOMPLETE_CONTROL_DEFAULT_CONFIG: XmAutocompleteControlConfig = 
         displayFn: '${name}',
         valueByKey: '${name}',
     },
+    formatBackendData: null,
     columns: [],
     height: 400,
     searchPlaceholder: 'global.rest-select-placeholder-search.simple',
