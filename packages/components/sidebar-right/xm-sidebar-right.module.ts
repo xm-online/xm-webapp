e014c49dfa6d09070eccdf4e3ae0700a9590f8b6
hennadii.varava <gennadiy.varava@gmail.com>
2023-07-21 11:40:48 +0300

    feat(right-sidebar): calculate minimum width and store in localstorage

--- packages/components/sidebar-right/xm-sidebar-right.module.ts
+++ packages/components/sidebar-right/xm-sidebar-right.module.ts
@@ -30,10 +30,15 @@ export class ContainerOutletDirective {
     host: {
         class: 'xm-sidebar-right',
     },
-    template: '<div class="resize-divider" #resizer></div><ng-container xmContainerOutlet></ng-container>',
+    template: `
+        <div class="resize-divider" #resizer></div>
+        <ng-container xmContainerOutlet></ng-container>
+    `,
 })
 export class XmSidebarRightComponent implements OnInit, OnDestroy {
+
     @ViewChild(ContainerOutletDirective, {static: true}) public xmContainerOutlet: ContainerOutletDirective;
+
     @ViewChild('resizer') public resizerElement: ElementRef;
 
     @HostBinding('style.width') public width: string;
@@ -50,7 +55,9 @@ export class XmSidebarRightComponent implements OnInit, OnDestroy {
             const vw = window.innerWidth / 100;
             const newWidthInPx = window.innerWidth - event.x;
             const newWidth = newWidthInPx / vw;
-            this.width = newWidth < 30 ? '30vw' : newWidth > 95 ? '95vw' : `${newWidth}vw`;
+            const min = this.minVW();
+            this.width = newWidth < min ? `${min}vw` : newWidth > 95 ? '95vw' : `${newWidth}vw`;
+            localStorage.setItem(this.getWidthStorageKey(), this.width);
         }
     }
 
@@ -89,7 +96,7 @@ export class XmSidebarRightComponent implements OnInit, OnDestroy {
 
         if (templateRef instanceof TemplateRef) {
             viewContainerRef.createEmbeddedView(templateRef);
-            this.openStyles(config.width || this.sidebarRightService.width);
+            this.openStyles(localStorage.getItem(this.getWidthStorageKey()) || config.width || this.sidebarRightService.width);
             return null;
         }
         return this.loadComponent(templateRef, config);
@@ -107,20 +114,36 @@ export class XmSidebarRightComponent implements OnInit, OnDestroy {
         });
         _.assign(component.instance, config.data);
 
-        this.openStyles(config.width || '300px');
+        this.openStyles(localStorage.getItem(this.getWidthStorageKey()) || config.width);
         return component.instance;
     }
 
     private openStyles(width: string): void {
-        const main: HTMLElement = document.getElementById('main');
-        main.style.marginRight = width;
-        this.width = width;
+        const min = this.minVW();
+        this.changeMainElementMarginBy(min + 'vw');
+
+        this.width = Math.max(parseFloat(width), min) + 'vw';
     }
 
     private closeStyles(): void {
-        const main: HTMLElement = document.getElementById('main');
-        main.style.marginRight = '0';
+        this.changeMainElementMarginBy('0');
         delete this.width;
+    }
+
+    private changeMainElementMarginBy(width: string): void {
+        const main: HTMLElement = document.getElementById('main');
+        main.style.marginRight = width;
+    }
+
+    private getWidthStorageKey(): string {
+        return 'xm-sidebar-rigth:width';
+    }
+
+    private minVW(): number {
+        const minContentWidthPx = 450;
+        const minByVW = window.innerWidth * 0.3;
+        const minWidthPx = Math.max(minByVW, minContentWidthPx);
+        return (minWidthPx * 100 / window.innerWidth);
     }
 }
 
