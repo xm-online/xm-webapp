9124c35aff10311851488325bb28053791b4927a
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2023-07-28 11:58:51 -0400

    refactor(components/date-range): add story
    Signed-off-by: Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>

---
+++ packages/components/date-range/xm-date-range.stories.ts
@@ -0,0 +1,29 @@
+import { moduleMetadata, StoryObj } from '@storybook/angular';
+import { DatePipe } from '@angular/common';
+import { XmDateRangeComponent } from '@xm-ngx/components/date-range/xm-date-range.component';
+
+export default {
+    title: 'Core/Date/Range',
+    component: XmDateRangeComponent,
+    decorators: [
+        moduleMetadata({
+            imports: [XmDateRangeComponent],
+            providers: [DatePipe],
+        }),
+    ],
+    parameters: {
+        layout: 'centered',
+    },
+};
+
+export const Default: StoryObj<XmDateRangeComponent> = {
+    args: {
+        config: {
+            format: 'yyyy-MM-dd HH:mm:ss',
+            separator: '-',
+            timezone: 'UTC',
+            locale: 'en-US',
+        },
+        value: [new Date(), new Date()],
+    },
+};
