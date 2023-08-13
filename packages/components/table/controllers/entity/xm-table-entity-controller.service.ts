879082cb172220167fedda0f10309cd7841cc12d
hennadii.varava <gennadiy.varava@gmail.com>
2023-08-13 12:08:03 +0300

    feat(layout): use dynamic controller instead

--- packages/components/table/controllers/entity/xm-table-entity-controller.service.ts
+++ packages/components/table/controllers/entity/xm-table-entity-controller.service.ts
@@ -1,26 +1,15 @@
-import { inject, Injectable } from '@angular/core';
-import {
-    PageEntityState,
-    PageEntityStore
-} from '@xm-ngx/ext/entity-webapp-ext/module/page-entity-widget/entity/page-entity-store';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
 
 @Injectable()
 export class XmTableEntityController<T = unknown> {
 
-    private pageEntityStore = inject<PageEntityStore<T>>(PageEntityStore);
-
     public entity$(): Observable<T> {
         // TODO:FEATURE: support entity in context
-        return this.pageEntityStore.state$().pipe(
-            filter(state => state === PageEntityState.LOADED),
-            map(() => this.pageEntityStore.entity()),
-        );
+        return null;
     }
 
     public update(entity: T): void {
         // TODO:FEATURE: support entity in context
-        this.pageEntityStore.update(entity);
     }
 }
