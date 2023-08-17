25e03a22dc6101e68669b6131376983807d40b31
Hennadii Varava <Sumragen@gmail.com>
2023-08-17 13:55:06 +0300

    fix(dynamic): relative path

--- packages/dynamic/widget/xm-dynamic-widget.directive.ts
+++ packages/dynamic/widget/xm-dynamic-widget.directive.ts
@@ -12,7 +12,7 @@ import { XmDynamicControllerDeclaration } from '../presentation/xm-dynamic-prese
 import { XM_DYNAMIC_COMPONENT_CONFIG } from '../src/dynamic.injectors';
 import {
     XmDynamicControllerInjectorFactoryService
-} from '@xm-ngx/dynamic/src/services/xm-dynamic-controller-injector-factory.service';
+} from '../src/services/xm-dynamic-controller-injector-factory.service';
 import * as _ from 'lodash';
 import { XmDynamicWidget } from './xm-dynamic-widget';
 import {
