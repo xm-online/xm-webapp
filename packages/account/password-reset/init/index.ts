a38553bc7e81eb5f96d94d00479f55139bcd6f92
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2023-08-22 18:53:54 -0700

    refactor(account): add index files
    Signed-off-by: Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>

---
+++ packages/account/password-reset/init/index.ts
@@ -0,0 +1,3 @@
+export * from './password-reset-init.service';
+export * from './password-reset-init.component';
+export * from './password-reset-init.route';
---
+++ packages/account/sign-up/index.ts
@@ -0,0 +1,2 @@
+export * from './sign-up.component';
+export * from './sign-up.route';
