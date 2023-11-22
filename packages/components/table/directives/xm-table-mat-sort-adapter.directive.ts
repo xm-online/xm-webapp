55d8b95e72f3b258b9a81d743f9cbe34e96d18e2
Андрей Гранковский <ahrankovskyi@itsmartflex.com>
2023-11-22 09:46:35 +0100

    fix(core): table sort update on default sort field by default

--- packages/components/table/directives/xm-table-mat-sort-adapter.directive.ts
+++ packages/components/table/directives/xm-table-mat-sort-adapter.directive.ts
@@ -1,7 +1,8 @@
 import { AfterViewInit, Directive, OnDestroy, Self } from '@angular/core';
-import { MatSort } from '@angular/material/sort';
+import { MatSort, SortDirection } from '@angular/material/sort';
 import { XmTableDirective } from './xm-table.directive';
 import { takeUntilOnDestroy, takeUntilOnDestroyDestroy } from '@xm-ngx/operators';
+import { take } from 'rxjs/operators';
 
 @Directive({
     selector: '[xmTableMatSortAdapter]',
@@ -18,16 +19,35 @@ export class XmTableMatSortAdapterDirective implements AfterViewInit, OnDestroy 
     }
 
     public ngAfterViewInit(): void {
+        this.observeSortChanges();
         this.observeTableAndUpdateMatSort();
     }
 
+
+
     public observeTableAndUpdateMatSort(): void {
         this.xmTableDirective.context$
-            .pipe(takeUntilOnDestroy(this))
+            .pipe(takeUntilOnDestroy(this),take(1))
             .subscribe((context) => {
-                this.matSort.active = context.collection.pageableAndSortable.sortBy as string;
-                this.matSort.direction = context.collection.pageableAndSortable.sortOrder;
+                const sortBy=context.collection.pageableAndSortable.sortBy as string;
+                const sortOrder =context.collection.pageableAndSortable.sortOrder;
+                if(this.isSortChanged(sortBy,sortOrder)) {
+                    return;
+                }
+                this.updateMatSort(sortBy,sortOrder);
             });
+
+    }
+
+    private updateMatSort(sortBy: string, sortOrder: SortDirection): void {
+        this.matSort.sort({ id: sortBy, start: sortOrder, disableClear: true });
+    }
+
+    private isSortChanged(sortBy: string, sortOrder: SortDirection): boolean {
+        return this.matSort.active === sortBy && this.matSort.direction === sortOrder;
+    }
+
+    private observeSortChanges(): void {
         this.matSort.sortChange
             .pipe(takeUntilOnDestroy(this))
             .subscribe(() => this.xmTableDirective.updatePagination());
@@ -38,3 +58,4 @@ export class XmTableMatSortAdapterDirective implements AfterViewInit, OnDestroy 
     }
 
 }
+
