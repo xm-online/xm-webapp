dcd811b2d5e61cee8f3bd9b1dd2cbcbd9acfc9e6
olensavchenko <olensavchenko@itsmartflex.com>
2023-12-05 11:35:39 +0200

    chore: shorten import

--- packages/translation/testing/mock-xm-translate.service.ts
+++ packages/translation/testing/mock-xm-translate.service.ts
@@ -1,7 +1,7 @@
-/** @public */
 import { Injectable } from '@angular/core';
 
 @Injectable()
+/** @public */
 export class MockXmTranslateService {
     public translate: (i: string) => string = (arg) => arg;
     public interpolate: (i: string) => string = (arg) => arg;
