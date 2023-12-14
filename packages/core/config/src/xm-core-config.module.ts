0ee74a0cb2db748ef10035b89f189594c1a974c6
Serhii Senko <serhii.senko@jevera.software>
2023-12-14 17:44:03 +0200

    Beta feature directive

--- packages/core/config/src/xm-core-config.module.ts
+++ packages/core/config/src/xm-core-config.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { XmPrivateUiConfigService } from './xm-private-ui-config.service';
 import { XmUiConfigService } from './xm-ui-config.service';
+import { XmBetaFeatureDirective } from "./xm-beta-feature.directive";
+import { CommonModule } from "@angular/common";
 
 @NgModule({
+    declarations: [XmBetaFeatureDirective],
+    exports: [XmBetaFeatureDirective],
     providers: [
         XmPrivateUiConfigService,
         XmUiConfigService,
+        CommonModule
     ],
 })
 export class XmCoreConfigModule {
