61b23b33d9a8d42ea1cc13e56b73c8de038021f1
Serhii Senko <serhii.senko@jevera.software>
2023-12-18 10:08:53 +0200

    Fix tests

--- packages/administration/dashboards-config/components/dashboard-edit/dashboard-edit.component.spec.ts
+++ packages/administration/dashboards-config/components/dashboard-edit/dashboard-edit.component.spec.ts
@@ -20,8 +20,8 @@ import { DashboardStore } from '@xm-ngx/dashboard';
 import { MockDashboardStore } from '@xm-ngx/core/dashboard/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MatDialogModule } from '@angular/material/dialog';
-import { XmUiConfigService } from "@xm-ngx/core/config";
-import { MockUiConfigService } from "@xm-ngx/core/config/testing";
+import { XmUiConfigService } from '@xm-ngx/core/config';
+import { MockUiConfigService } from '@xm-ngx/core/config/testing';
 
 @Component({
     selector: 'xm-text-control, xm-ace-editor-control',
