9549ae516a04f88ad520f419568a19a1f39acc75
otrokhymenko <otrokhymenko@itsmartflex.com>
2024-01-25 16:07:36 +0200

    fix(dayjs): fix build

--- packages/operators/src/dayjs-util.ts
+++ packages/operators/src/dayjs-util.ts
@@ -1,10 +1,10 @@
-import * as dayjsLib from 'dayjs';
+import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
 import isBetween from 'dayjs/plugin/isBetween'
 
-dayjsLib.extend(utc);
-dayjsLib.extend(timezone);
-dayjsLib.extend(isBetween);
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.extend(isBetween);
 
-export const dayjs = dayjsLib;
+export { dayjs };
