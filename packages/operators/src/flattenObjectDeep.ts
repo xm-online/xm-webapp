acaf61512867450a9dee22e9d4cbf59ddc3b6bbb
Андрей Гранковский <ahrankovskyi@itsmartflex.com>
2024-01-31 18:59:53 +0100

    fix(core): table query-params  remove optimization typification

--- packages/operators/src/flattenObjectDeep.ts
+++ packages/operators/src/flattenObjectDeep.ts
@@ -1,6 +1,6 @@
 import { forEach, isArray, isPlainObject, set } from 'lodash';
 
-const flatten = (collection, prefix = '', suffix = '',result) => {
+const flatten = (collection: object, prefix: string = '', suffix: string = '', result: object): object => {
     forEach(collection, (value, key) => {
         const path = `${prefix}${key}${suffix}`;
         if (isArray(value)) {
@@ -14,13 +14,13 @@ const flatten = (collection, prefix = '', suffix = '',result) => {
     return result;
 };
 
-export const flattenObjectDeep = (obj = {}, prefix: string): Record<string, unknown> => {
+export const flattenObjectDeep = (obj: object = {}, prefix: string): Record<string, unknown> => {
     const result = {};
     flatten(obj, prefix,'',result);
     return result;
 };
 
-export const unFlattenObjectDeep = (obj = {}, prefix: string): object => {
+export const unFlattenObjectDeep = (obj: object = {}, prefix: string): object => {
     const unFlattenObject = (obj, prefix: string = '') =>
         Object.keys(obj).reduce((res, key) => {
             const newKey = key.startsWith(prefix) ? key.substring(prefix.length) : key;
