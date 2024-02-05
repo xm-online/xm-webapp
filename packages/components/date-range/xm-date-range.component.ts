aee7aa52ae18d41d9e4b2acef6bedfdf7aca2aff
otrokhymenko <otrokhymenko@itsmartflex.com>
2024-02-05 16:04:31 +0200

    fix(xm-date-range): fix date locale on change acc settings

--- packages/components/date-range/xm-date-range.component.ts
+++ packages/components/date-range/xm-date-range.component.ts
@@ -1,8 +1,16 @@
 import { DatePipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    Component, inject,
+    Input,
+    ViewEncapsulation,
+    OnInit
+} from '@angular/core';
 import { XmDynamicPresentation, } from '@xm-ngx/dynamic';
 import { XM_DATE_CONFIG_DEFAULT, XmDateConfig, XmDateValue } from '@xm-ngx/components/date';
 import { Defaults } from '@xm-ngx/operators';
+import { XmUserService } from '@xm-ngx/core/user';
+import { take } from 'rxjs/operators';
 
 export interface XmDateRangeConfig extends XmDateConfig {
     separator: string;
@@ -22,12 +30,25 @@ export const XM_DATE_RANGE_CONFIG_DEFAULT: XmDateRangeConfig = {
     ],
     standalone: true,
     template: `
-        {{ value.from | date : config.format : config.timezone : config.locale }}{{config.separator}}{{ value.to | date : config.format : config.timezone : config.locale }}
+        {{ value.from | date : config.format : config.timezone : (config.locale || locale) }}{{config.separator}}{{ value.to | date : config.format : config.timezone : (config.locale || locale) }}
     `,
     encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.Default,
 })
-export class XmDateRangeComponent implements XmDynamicPresentation<XmDateRangeValue, XmDateRangeConfig> {
-    @Input() @Defaults({ from: '', to: '' }) public value: XmDateRangeValue;
+export class XmDateRangeComponent implements XmDynamicPresentation<XmDateRangeValue, XmDateRangeConfig>, OnInit {
+    @Input() @Defaults({from: '', to: ''}) public value: XmDateRangeValue;
     @Input() @Defaults(XM_DATE_RANGE_CONFIG_DEFAULT) public config: XmDateRangeConfig;
+    public locale: string = 'en';
+
+    private xmUserService: XmUserService = inject(XmUserService);
+
+    public ngOnInit(): void {
+        this.xmUserService.user$()
+            .pipe(
+                take(1)
+            )
+            .subscribe((user) => {
+                this.locale = user.langKey;
+            });
+    }
 }
