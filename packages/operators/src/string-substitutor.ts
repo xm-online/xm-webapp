4d2222f315b69376ee80e0917cb7705c81119e72
sergeysenja1992 <sergeysenja1992@users.noreply.github.com>
2024-02-29 15:56:10 +0200

    Update string-substitutor.ts

--- packages/operators/src/string-substitutor.ts
+++ packages/operators/src/string-substitutor.ts
@@ -4,7 +4,7 @@ export function stringSubstitute(template: string, values: object): string {
     return template.replace(/\$\{(.+?)(:-.+?)?\}/g, (match, key, defaultValue) => {
         key = key.trim();
         defaultValue = defaultValue ? defaultValue.slice(2).trim() : '';
-        const value = _.get(values, key)
+        const value = _.get(values, key);
         return _.isNil(value) ? defaultValue : value;
     });
 }
