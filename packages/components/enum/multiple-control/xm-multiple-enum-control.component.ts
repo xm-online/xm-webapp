3062c43f5865f43324c3341662855694881d40b2
olensavchenko <olensavchenko@itsmartflex.com>
2024-07-29 15:11:26 +0300

    fix(multiple-enum): fix clear value

--- packages/components/enum/multiple-control/xm-multiple-enum-control.component.ts
+++ packages/components/enum/multiple-control/xm-multiple-enum-control.component.ts
@@ -37,7 +37,7 @@ export const XM_MULTIPLE_ENUM_CONTROL_OPTIONS_DEFAULT: XmMultipleEnumControlOpti
     selector: 'xm-multiple-enum-control',
     template: `
         <mat-form-field>
-            <mat-label>{{config?.title | translate}}</mat-label>
+            <mat-label>{{ config?.title | translate }}</mat-label>
             <mat-select [formControl]="control"
                         [required]="config.required"
                         [id]="config.id"
@@ -47,44 +47,46 @@ export const XM_MULTIPLE_ENUM_CONTROL_OPTIONS_DEFAULT: XmMultipleEnumControlOpti
                 <mat-select-trigger>
                     <ng-container *ngIf="itemsMap && itemsMap[value[0] + '']">
                         <mat-icon style="vertical-align: middle"
-                                  *ngIf="itemsMap[value[0] + '']?.icon">{{itemsMap[value[0] + ''].icon}}</mat-icon>
-                        {{(itemsMap[value[0] + ''].title | translate) || ''}}
+                                  *ngIf="itemsMap[value[0] + '']?.icon">{{ itemsMap[value[0] + ''].icon }}
+                        </mat-icon>
+                        {{ (itemsMap[value[0] + ''].title | translate) || '' }}
                     </ng-container>
                     <span *ngIf="control.value?.length > 1" class="small">
-                        (+{{control.value?.length - 1}} {{(control.value?.length === 2 ? "xm-enum.other" : "xm-enum.others")  | translate}}
+                        (+{{ control.value?.length - 1 }} {{ (control.value?.length === 2 ? "xm-enum.other" : "xm-enum.others")  | translate }}
                         )
                         </span>
                 </mat-select-trigger>
                 <div class="empty-option" *ngIf="config?.clearButton" [hidden]="!control.value" (click)="deselect()">
                     <mat-icon>close</mat-icon>
                     <div>
-                        {{'ext-entity.common.cancel' | translate}}
+                        {{ 'ext-entity.common.cancel' | translate }}
                     </div>
                 </div>
 
                 <ng-template ngFor [ngForOf]="itemsList" let-item>
                     <mat-option [value]="item.value" *xmPermission="item.permissions || []">
-                        <mat-icon *ngIf="item.icon">{{item.icon}}</mat-icon>
-                        {{item.title | translate}}
+                        <mat-icon *ngIf="item.icon">{{ item.icon }}</mat-icon>
+                        {{ item.title | translate }}
                     </mat-option>
                 </ng-template>
             </mat-select>
 
-            <mat-error *xmControlErrors="control?.errors; message as message">{{message}}</mat-error>
+            <mat-error *xmControlErrors="control?.errors; message as message">{{ message }}</mat-error>
 
             <mat-hint [hint]="config.hint"></mat-hint>
         </mat-form-field>
     `,
     styles: [`
-        .empty-option {
-            cursor: pointer;
-            padding: 9px 13px;
-            align-items: center;
-            display: flex;
-        }
-        .empty-option:hover {
-            background: rgba(0, 0, 0, 0.04);
-        }
+      .empty-option {
+        cursor: pointer;
+        padding: 9px 13px;
+        align-items: center;
+        display: flex;
+      }
+
+      .empty-option:hover {
+        background: rgba(0, 0, 0, 0.04);
+      }
     `],
     imports: [
         XmTranslationModule,
@@ -107,12 +109,13 @@ export class XmMultipleEnumControl
     extends NgFormAccessor<XmEnumValue[] | undefined>
     implements XmDynamicControl<XmEnumValue[] | undefined, XmMultipleEnumControlOptions>, OnInit {
     public itemsList: XmEnumControlOptionsItem[];
-    public itemsMap: {[value: string]: XmEnumControlOptionsItem};
+    public itemsMap: { [value: string]: XmEnumControlOptionsItem };
     private _config: XmMultipleEnumControlOptions = clone(XM_MULTIPLE_ENUM_CONTROL_OPTIONS_DEFAULT);
 
     constructor(@Optional() @Self() public ngControl: NgControl) {
         super(ngControl);
     }
+
     public get value(): XmEnumValue[] {
         return this._value == undefined ? [] : this._value;
     }
@@ -153,13 +156,12 @@ export class XmMultipleEnumControl
     }
 
     private setInitValue(): void {
-        const { initValue } = this.config;
+        const {initValue} = this.config;
 
-        if (!initValue) {
-            return;
+        if (initValue?.length) {
+            this.value = initValue;
         }
 
-        this.value = initValue;
-        this.control.patchValue(this.value);
+        this.control.patchValue(this.value?.length ? this.value : null );
     }
 }
