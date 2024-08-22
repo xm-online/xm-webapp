b3761ba5643378d47425821e9a8ad42553e0f32a
hvarava <hvarava@itsmartflex.com>
2024-08-22 11:29:41 +0300

    feat(dayjs): custom parse format

--- packages/operators/src/dayjs-util.ts
+++ packages/operators/src/dayjs-util.ts
@@ -1,10 +1,12 @@
 import dayjs from 'dayjs';
-import utc from 'dayjs/plugin/utc';
-import timezone from 'dayjs/plugin/timezone';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
 import isBetween from 'dayjs/plugin/isBetween';
+import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.extend(isBetween);
+dayjs.extend(customParseFormat);
 
 export { dayjs };
