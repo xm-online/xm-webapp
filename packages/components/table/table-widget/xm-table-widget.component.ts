fe507ac3ab39342984ee016774e0f51038db8ac1
hvarava <hvarava@itsmartflex.com>
2024-11-25 13:25:41 +0200

    fix(sticky-overlaps): recalculate sticky styles

--- packages/components/table/table-widget/xm-table-widget.component.ts
+++ packages/components/table/table-widget/xm-table-widget.component.ts
@@ -1,39 +1,39 @@
 import { AsyncPipe, JsonPipe, NgClass, NgForOf, NgIf } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatPaginatorIntl, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
-import { MatTableModule } from '@angular/material/table';
+import { MatTable, MatTableModule } from '@angular/material/table';
 import { injectByKey } from '@xm-ngx/dynamic';
 import { XmTranslatePipe } from '@xm-ngx/translation';
 import { defaultsDeep } from 'lodash';
-import { XmTableColumnDynamicCellComponent } from '../columns/xm-table-column-dynamic-cell.component';
-import { XmTableDynamicColumnComponent } from '../columns/xm-table-dynamic-column.component';
-import { XmTableSelectionHeaderComponent } from '../components/selection-header/xm-table-selection-header.component';
-import { XmTableActionsButtonsComponent } from '../components/xm-table-actions-buttons.component';
-import { XmTableEmptyComponent } from '../components/xm-table-empty.component';
-import { XmTableFilterButtonComponent } from '../components/xm-table-filter-button.component';
-import { XmTableFilterChipsComponent } from '../components/xm-table-filter-chips.component';
-import { XmTableHeaderComponent } from '../components/xm-table-header.component';
-import { XmTableLoadingColumnComponent } from '../components/xm-table-loading-column.component';
-import { XmTableLoadingComponent } from '../components/xm-table-loading.component';
-import { XmTableSelectionColumnComponent } from '../components/xm-table-selection-column.component';
+import { TableExpand } from '../animations/xm-table-widget.animation';
 import {
     IXmTableCollectionController,
     XM_TABLE_CONTROLLERS,
     XmTableCollectionControllerResolver
 } from '../collections';
+import { XmTableColumnDynamicCellComponent } from '../columns/xm-table-column-dynamic-cell.component';
+import { XmTableDynamicColumnComponent } from '../columns/xm-table-dynamic-column.component';
+import { XmTableSelectionHeaderComponent } from '../components/selection-header/xm-table-selection-header.component';
+import { XmTableActionsButtonsComponent } from '../components/xm-table-actions-buttons.component';
+import { XmTableEmptyComponent } from '../components/xm-table-empty.component';
+import { XmTableExpandPanelButtonComponent } from '../components/xm-table-expand-panel-button.component';
+import { XmTableFilterButtonComponent } from '../components/xm-table-filter-button.component';
+import { XmTableFilterChipsComponent } from '../components/xm-table-filter-chips.component';
+import { XmTableFilterInlineComponent } from '../components/xm-table-filter-inline.component';
+import { XmTableHeaderComponent } from '../components/xm-table-header.component';
+import { XmTableLoadingColumnComponent } from '../components/xm-table-loading-column.component';
+import { XmTableLoadingComponent } from '../components/xm-table-loading.component';
+import { XmTableQuickFilterInlineComponent } from '../components/xm-table-quick-filter-inline.component';
+import { XmTableSelectionColumnComponent } from '../components/xm-table-selection-column.component';
 import { XmTableMatPaginatorAdapterDirective } from '../directives/xm-table-mat-paginator-adapter.directive';
 import { XmTableMatSortAdapterDirective } from '../directives/xm-table-mat-sort-adapter.directive';
 import { XmTableSelectionDirective } from '../directives/xm-table-selection.directive';
 import { XmTableDirective } from '../directives/xm-table.directive';
 import { XM_TABLE_CONFIG_DEFAULT } from '../directives/xm-table.model';
-import { XM_TABLE_WIDGET_CONFIG_DEFAULT, XmTableWidgetConfig } from './xm-table-widget.config';
-import { XmTableExpandPanelButtonComponent } from '../components/xm-table-expand-panel-button.component';
-import { TableExpand } from '../animations/xm-table-widget.animation';
-import { XmTableFilterInlineComponent } from '../components/xm-table-filter-inline.component';
 import { XmTableMatPaginatorInt } from './table.mat-paginator-int';
-import { XmTableQuickFilterInlineComponent } from '../components/xm-table-quick-filter-inline.component';
+import { XM_TABLE_WIDGET_CONFIG_DEFAULT, XmTableWidgetConfig } from './xm-table-widget.config';
 
 function getConfig(value: Partial<XmTableWidgetConfig>): XmTableWidgetConfig {
     const config = defaultsDeep({}, value, XM_TABLE_WIDGET_CONFIG_DEFAULT, XM_TABLE_CONFIG_DEFAULT) as XmTableWidgetConfig;
@@ -94,6 +94,14 @@ function getConfig(value: Partial<XmTableWidgetConfig>): XmTableWidgetConfig {
 export class XmTableWidget {
 
     private collectionController: IXmTableCollectionController<unknown> = injectByKey<IXmTableCollectionController<unknown>>('collection', {optional: true});
+
+    @ViewChild(MatTable) public table: MatTable<any>;
+
+    public ngAfterViewChecked(): void {
+        if (this.table) {
+            this.table.updateStickyColumnStyles();
+        }
+    }
 
     constructor(
         private collectionControllerResolver: XmTableCollectionControllerResolver) {
