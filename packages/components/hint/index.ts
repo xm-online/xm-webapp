b18acd52906013b05b42616d4a620c319867c60f
hsalatsynska <hsalatsynska@itsmartflex.com>
2025-05-27 13:49:32 +0300

    Fix build, added export for const

--- packages/components/hint/index.ts
+++ packages/components/hint/index.ts
@@ -4,3 +4,4 @@ export { HintComponent } from './hint.component';
 export { HintText } from './hint.interface';
 export { HintSwitchComponent, HINT_SWITCH_CONFIG, HintSwitchConfig } from './hint-switch.component';
 export { HintPopoverComponent } from './hint-popover.component';
+export { DIALOG_OPENED_ACTION } from './hint-popover.component';
