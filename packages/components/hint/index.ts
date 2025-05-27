55d32748b4cff3ae3f5d8e108c7595b2c42ded04
salatsynskahv <salatsynskahv@gmail.com>
2025-05-27 13:55:30 +0300

    Update index.ts

--- packages/components/hint/index.ts
+++ packages/components/hint/index.ts
@@ -3,5 +3,4 @@ export { HintService } from './hint.service';
 export { HintComponent } from './hint.component';
 export { HintText } from './hint.interface';
 export { HintSwitchComponent, HINT_SWITCH_CONFIG, HintSwitchConfig } from './hint-switch.component';
-export { HintPopoverComponent } from './hint-popover.component';
-export { DIALOG_OPENED_ACTION } from './hint-popover.component';
+export { HintPopoverComponent, DIALOG_OPENED_ACTION } from './hint-popover.component';
