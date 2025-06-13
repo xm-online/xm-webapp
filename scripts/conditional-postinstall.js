0c50094265398462351316350e80c9964f2afd98
andriiminaiev <aminaiev@itsmartflex.com>
2025-06-13 15:51:32 +0300

    add conditional-postinstall.js

---
+++ scripts/conditional-postinstall.js
@@ -0,0 +1,11 @@
+if (!process.env.SKIP_POSTINSTALL) {
+    const {execSync} = require('child_process');
+    try {
+        execSync('npm run build:packages', {stdio: 'inherit'});
+    } catch (error) {
+        console.error('Error executing build:packages:', error);
+        process.exit(1);
+    }
+} else {
+    console.log('NX_RUN is not set, skipping build:packages');
+}
