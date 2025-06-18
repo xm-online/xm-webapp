910101f275659a32265d867e26af42db8aa7cdb9
Bogdan Tushevskyi <tushevskuy@gmail.com>
2025-06-18 14:21:14 +0300

    Fix tests with NullInjectorError

--- packages/components/logo/image-logo/image-logo.component.spec.ts
+++ packages/components/logo/image-logo/image-logo.component.spec.ts
@@ -3,6 +3,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { XmSessionService } from '@xm-ngx/core';
 import { XmTranslationTestingModule } from '@xm-ngx/translation/testing';
 import { of } from 'rxjs';
+import { Principal } from '@xm-ngx/core/user';
+import { AccountService } from '@xm-ngx/core/user';
 
 import { ImageLogoComponent } from './image-logo.component';
 import { RouterTestingModule } from '@angular/router/testing';
@@ -15,7 +17,11 @@ describe('ImageLogoComponent', () => {
         await TestBed.configureTestingModule({
             schemas: [NO_ERRORS_SCHEMA],
             imports: [ImageLogoComponent, RouterTestingModule, XmTranslationTestingModule],
-            providers: [{ provide: XmSessionService, useValue: { get: () => of({}) } }],
+            providers: [
+                { provide: XmSessionService, useValue: { get: () => of({}) } },
+                { provide: Principal, useValue: {} },
+                { provide: AccountService, useValue: {} },
+            ],
         })
             .compileComponents();
     });
