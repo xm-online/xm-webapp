97b4402bf1b9b1d7852dbdca208644ac643cec66
Bogdan Tushevskyi <tushevskuy@gmail.com>
2025-06-19 15:11:57 +0300

    Update side menu config for hide menu

--- src/app/layouts/components/layout-wrapper/layout-wrapper.component.ts
+++ src/app/layouts/components/layout-wrapper/layout-wrapper.component.ts
@@ -11,6 +11,7 @@ import { XmUIConfig, XmUiConfigService } from '@xm-ngx/core/config';
 
 interface XmMainConfig extends XmUIConfig {
     main: { layout: XmDynamicLayout[] };
+    navbar: { hidden: boolean };
 }
 
 @Component({
@@ -32,6 +33,7 @@ export class LayoutWrapperComponent implements OnInit, AfterViewInit, OnDestroy 
     public isMobileScreen: boolean;
     public menuCategories$: Observable<MenuCategory[]>;
     public mainLayout: XmDynamicLayout[];
+    public isNavbarHidden: boolean;
 
     @ViewChild('sidenav') public sidenav: MatSidenav;
 
@@ -53,6 +55,7 @@ export class LayoutWrapperComponent implements OnInit, AfterViewInit, OnDestroy 
             takeUntilOnDestroy(this),
         ).subscribe((config) => {
             this.mainLayout = config.main?.layout || null;
+            this.isNavbarHidden = config.navbar?.hidden || false;
         });
     }
 
