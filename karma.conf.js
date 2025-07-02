ca9cc4071b6710db5e71b493ea239bf9556f042b
andriiminaiev <aminaiev@itsmartflex.com>
2025-07-02 16:27:00 +0300

    update to current main version

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,38 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function(config) {
+module.exports = function (config) {
+    const ForceExitReporter = function (baseReporterDecorator) {
+        baseReporterDecorator(this);
+
+        this.onRunComplete = function (browsers, results) {
+            const exitCode = results.failed ? 1 : 0;
+            if (exitCode !== 0) {
+                console.log(`\nTests failed. Forcing exit with code ${exitCode}.`);
+                process.exit(exitCode);
+            } else if (config.singleRun) {
+                // Завершаем процесс только если singleRun=true
+                console.log(`\nAll tests passed. Forcing exit with code ${exitCode}.`);
+                process.exit(exitCode);
+            }
+        };
+        this.onBrowserError = function () {
+        };
+        this.onBrowserStart = function () {
+        };
+        this.onBrowserComplete = function () {
+        };
+        this.specSuccess = function () {
+        };
+        this.specFailure = function () {
+        };
+        this.specSkipped = function () {
+        };
+    };
+
+    ForceExitReporter.$inject = ['baseReporterDecorator'];
+
+
     config.set({
         execArgv: ['--max_old_space_size=8096'],
         basePath: '',
@@ -12,21 +43,22 @@ module.exports = function(config) {
             require('karma-jasmine-html-reporter'),
             require('karma-coverage'),
             require('@angular-devkit/build-angular/plugins/karma'),
+            {'reporter:force-exit': ['factory', ForceExitReporter]}
         ],
         client: {
-            clearContext: false, // leave Jasmine Spec Runner output visible in browser
+            clearContext: false,
         },
         jasmineHtmlReporter: {
-            suppressAll: true, // removes the duplicated traces
+            suppressAll: true,
         },
         coverageReporter: {
             dir: require('path').join(__dirname, '/coverage/'),
             subdir: '.',
             reporters: [
-                { type: 'lcov' },
+                {type: 'lcov'},
             ],
         },
-        reporters: ['progress', 'kjhtml'],
+        reporters: ['progress', 'kjhtml', 'force-exit'],
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
