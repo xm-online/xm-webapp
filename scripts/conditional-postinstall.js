197a54ebafb969c7884ec0e24d81cdea5836a1fd
andriiminaiev <aminaiev@itsmartflex.com>
2025-08-04 12:16:41 +0300

    Support both build options NX and old one

--- scripts/conditional-postinstall.js
+++ scripts/conditional-postinstall.js
@@ -1,11 +1,18 @@
-if (!process.env.SKIP_POSTINSTALL) {
-    const {execSync} = require('child_process');
-    try {
-        execSync('npm run build:packages', {stdio: 'inherit'});
-    } catch (error) {
-        console.error('Error executing build:packages:', error);
-        process.exit(1);
-    }
-} else {
-    console.info('NX_RUN is not set, skipping build:packages');
+// if (!process.env.SKIP_POSTINSTALL) {
+//     const {execSync} = require('child_process');
+//     try {
+//         execSync('npm run build:packages', {stdio: 'inherit'});
+//     } catch (error) {
+//         console.error('Error executing build:packages:', error);
+//         process.exit(1);
+//     }
+// } else {
+//     console.info('NX_RUN is not set, skipping build:packages');
+// }
+const {execSync} = require('child_process');
+try {
+    execSync('npm run build:packages', {stdio: 'inherit'});
+} catch (error) {
+    console.error('Error executing build:packages:', error);
+    process.exit(1);
 }
