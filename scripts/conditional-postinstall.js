704e37d76f1532cf1dde52e5b3707b87db4991a1
andriiminaiev <aminaiev@itsmartflex.com>
2025-08-06 12:58:10 +0300

    Support both build options NX and old one

--- scripts/conditional-postinstall.js
+++ scripts/conditional-postinstall.js
@@ -1,3 +1,11 @@
+// 🛑 CRITICAL WARNING: LOCAL DEVELOPMENT ONLY 🛑
+//
+// DO NOT COMMIT changes to this file (conditional-postinstall.js).
+// This modification is intended EXCLUSIVELY for your local environment.
+//
+// ---> ALWAYS REVERT THIS FILE TO ITS ORIGINAL STATE BEFORE COMMITTING. <---
+// Failure to do so WILL break the build in CI.
+
 const NEW_VERSION_BUILD = process.env.NEW_VERSION_BUILD ?? false;
 const IS_CI = process.env.CI;
 const {execSync} = require('child_process');
@@ -12,7 +20,7 @@ if (NEW_VERSION_BUILD === 'false' || !NEW_VERSION_BUILD) {
     try {
         execSync('npm run build:nx-packages', {stdio: 'inherit'});
     } catch (error) {
-        console.error('Error executing build:packages:', error);
+        console.error('Error executing build:nx-packages:', error);
         process.exit(1);
     }
 }
