bc224b811b08bdb01c9d3d1cd4fe3d2699d74953
denisgolubev <denisogolubev@gmail.com>
2025-11-07 18:19:09 +0200

    NX: Scripts update for livereload and prod build

--- scripts/conditional-postinstall.js
+++ scripts/conditional-postinstall.js
@@ -6,8 +6,7 @@
 // ---> ALWAYS REVERT THIS FILE TO ITS ORIGINAL STATE BEFORE COMMITTING. <---
 // Failure to do so WILL break the build in CI.
 
-const NEW_VERSION_BUILD = process.env.NEW_VERSION_BUILD ?? false;
-const IS_CI = process.env.CI;
+const NEW_VERSION_BUILD = process.env.NEW_VERSION_BUILD ?? true;
 const {execSync} = require('child_process');
 if (NEW_VERSION_BUILD === 'false' || !NEW_VERSION_BUILD) {
     try {
@@ -16,7 +15,7 @@ if (NEW_VERSION_BUILD === 'false' || !NEW_VERSION_BUILD) {
         console.error('Error executing build:packages:', error);
         process.exit(1);
     }
-} else if (NEW_VERSION_BUILD && !IS_CI) {
+} else if (NEW_VERSION_BUILD === 'true' || NEW_VERSION_BUILD) {
     try {
         execSync('npm run build:nx-packages', {stdio: 'inherit'});
     } catch (error) {
