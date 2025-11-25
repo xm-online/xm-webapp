cb9836bbd2b98ef057859850dc3aabc6131844ef
Olensavchenko <91541873+Olensavchenko@users.noreply.github.com>
2025-11-25 15:44:02 +0200

    Update null check to include undefined check

--- packages/components/text/text-view/xm-text-view.component.ts
+++ packages/components/text/text-view/xm-text-view.component.ts
@@ -37,7 +37,7 @@ export type PrimitiveOrTranslate = Primitive & Translate;
                   xmValue>
                 @if (isArrayValue(value)) {
                     <span>{{ transformValue(value) }}</span>
-                } @else if (value != null) {
+                } @else if (value !== null && value !== undefined) {
                     <span>{{ value | translate }}</span>
                 } @else {
                     {{ config.emptyValue | translate }}
