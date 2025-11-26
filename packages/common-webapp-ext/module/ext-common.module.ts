6754585c2395566a061a7932d1c86e2e98a293ac
I.Chupryna <ievgen.chupryna@xmedigital.com>
2025-11-26 19:23:44 +0000

    Removing unused widgets from common-webapps

--- packages/common-webapp-ext/module/ext-common.module.ts
+++ packages/common-webapp-ext/module/ext-common.module.ts
@@ -5,18 +5,9 @@ import { CovalentTextEditorModule } from '@covalent/text-editor';
 import { XmDynamicModule } from '@xm-ngx/dynamic';
 
 import { XmSharedModule } from '@xm-ngx/shared';
-import { ClockWidgetComponent } from '../clock-widget/clock-widget.component';
-import { ExchangeWidgetComponent } from '../exchange-widget/exchange-widget.component';
-import { FinanceService } from '../exchange-widget/finance.service';
 import { IframeWidgetComponent } from '../iframe-widget/iframe-widget.component';
 import { MdWidgetComponent } from '../md-widget/md-widget.component';
-import { FeedService } from '../news-widget/feed.service';
-import { NewsWidgetComponent } from '../news-widget/news-widget.component';
 import { SignInUpWidgetComponent } from '../sign-in-up-widget/sign-in-up-widget.component';
-import { TwitterTimelineWidgetComponent } from '../twitter-timeline-widget/twitter-timeline-widget.component';
-import { TwitterTimelineService } from '../twitter-timeline-widget/twitter-timeline.service';
-import { WeatherWidgetComponent } from '../weather-widget/weather-widget.component';
-import { WeatherService } from '../weather-widget/weather.service';
 import { WelcomeWidgetComponent } from '../welcome-widget/welcome-widget.component';
 import { SignInUpV2WidgetComponent } from '../sign-in-up-v2-widget/sign-in-up-v2-widget.component';
 import { LoginV2Component } from '../sign-in-up-v2-widget/login-v2/login-v2.component';
@@ -28,15 +19,10 @@ import { LoginTfaComponent } from '../sign-in-up-v2-widget/login-tfa/login-tfa.c
         XmSharedModule,
         HttpClientJsonpModule,
         XmDynamicModule.forChild([
-            { selector: 'xm-widget-clock', loadChildren: () => ClockWidgetComponent },
-            { selector: 'xm-widget-exchange-calculator', loadChildren: () => ExchangeWidgetComponent },
             { selector: 'xm-widget-iframe', loadChildren: () => IframeWidgetComponent },
             { selector: 'xm-widget-md', loadChildren: () => MdWidgetComponent },
-            { selector: 'xm-widget-news', loadChildren: () => NewsWidgetComponent },
             { selector: 'xm-widget-sign-in-up', loadChildren: () => SignInUpWidgetComponent },
             { selector: 'xm-widget-sign-in-up-v2', loadChildren: () => SignInUpV2WidgetComponent },
-            { selector: 'xm-widget-twitter-timeline', loadChildren: () => TwitterTimelineWidgetComponent },
-            { selector: 'xm-widget-weather', loadChildren: () => WeatherWidgetComponent },
             { selector: 'xm-widget-welcome', loadChildren: () => WelcomeWidgetComponent },
             { selector: 'xm-login-v2', loadChildren: () => LoginV2Component },
             { selector: 'xm-login-tfa', loadChildren: () => LoginTfaComponent },
@@ -44,21 +30,12 @@ import { LoginTfaComponent } from '../sign-in-up-v2-widget/login-tfa/login-tfa.c
         CovalentTextEditorModule,
     ],
     declarations: [
-        ClockWidgetComponent,
-        ExchangeWidgetComponent,
         IframeWidgetComponent,
         MdWidgetComponent,
-        NewsWidgetComponent,
         SignInUpWidgetComponent,
-        TwitterTimelineWidgetComponent,
-        WeatherWidgetComponent,
         WelcomeWidgetComponent,
     ],
     providers: [
-        FeedService,
-        FinanceService,
-        TwitterTimelineService,
-        WeatherService,
     ],
 })
 export class ExtCommonModule {
--- packages/common-webapp-ext/news-widget/feed-info.model.ts
+++
@@ -1,7 +0,0 @@
-export interface FeedInfo {
-    title: string;
-    link: string;
-    author: string;
-    description: string;
-    image: string;
-}
