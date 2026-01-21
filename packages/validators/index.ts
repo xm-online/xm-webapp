1e195a5af739de97cdc72c367095709d6702c26e
vromaniuk <vherever@gmail.com>
2026-01-21 11:41:30 +0200

    fix: date-time-range validator imports update

--- packages/validators/index.ts
+++ packages/validators/index.ts
@@ -1,3 +1,4 @@
 export { maxDateValidator } from './src/max-date.validator';
 export { yearsValidator } from './src/years.validator';
 export { uniqueFieldsValidator } from './src/unique-fields-validator';
+export { dateTimeRangeValidator } from './src/date-time-range.validator';
