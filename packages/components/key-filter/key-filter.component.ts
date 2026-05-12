41e0c00c969f546c3a2d9063811d2b434de25ced
hsalatsynska <hsalatsynska@itsmartflex.com>
2026-05-12 15:27:41 +0300

    Feat: Wrap value in array in key-filter component

--- packages/components/key-filter/key-filter.component.ts
+++ packages/components/key-filter/key-filter.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component, Input, OnChanges } from '@angular/c
 import { KeyFilterController, XmKeyFilterConfig, XmKeyFilterValue } from '../../components/key-filter/key-filter.model';
 import { injectByKey, XmDynamicModule, XmDynamicPresentation } from '@xm-ngx/dynamic';
 import { searchNestedByPredicate } from '@xm-ngx/operators';
-import { get } from 'lodash';
+import { get, isArray } from 'lodash';
 
 @Component({
     standalone: true,
@@ -52,7 +52,7 @@ export class XmKeyFilterComponent implements XmDynamicPresentation<XmKeyFilterVa
     }
 
     public prepareValue(): XmKeyFilterValue {
-        const value = this.value ?? [];
+        const value = this.value ? (isArray(this.value) ? this.value: [this.value]): [];
 
         if (this.keyFilterController?.prepareValue) {
             return this.keyFilterController.prepareValue(this.value, this.config);
